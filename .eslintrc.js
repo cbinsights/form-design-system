28709e1c2bb58571298a9adaa854345bec2ad575
Adam Detrick <adetrick@cbinsights.com>
2018-08-20 15:52:28 -0400

    fix eslint and jest

---
+++ .eslintrc.js
@@ -0,0 +1,38 @@
+module.exports = {
+  root: true,
+  parser: 'babel-eslint',
+  parserOptions: {
+    ecmaVersion: 6,
+    ecmaFeatures: {
+      jsx: true,
+      experimentalObjectRestSpread: true
+    },
+    sourceType: 'module'
+  },
+  extends: [
+    'airbnb-base',
+    'plugin:react/recommended',
+    'prettier'
+  ],
+  plugins: [
+    'react',
+    'jest'
+  ],
+  rules: {
+    'react/jsx-uses-react': 'error',
+    'react/jsx-uses-vars': 'error'
+  },
+  env: {
+    'jest/globals': true
+  },
+  overrides: [
+    {
+      files: [
+        '*.test.js'
+      ],
+      'rules': {
+        'import/no-unresolved': false
+      }
+    }
+  ]
+}
