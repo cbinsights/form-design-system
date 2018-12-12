1ac01467674ad974f3a5abc613151c56910e3df2
Adam Detrick <adetrick@cbinsights.com>
2018-12-12 16:21:53 -0500

    specify react 16.3

--- scripts/setupTests.js
+++
@@ -1,5 +0,0 @@
-/* eslint-disable */
-import Enzyme from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16.3';
-
-Enzyme.configure({ adapter: new Adapter() });
---
+++ util/setupTests.js
@@ -0,0 +1,9 @@
+/* eslint-disable */
+import Enzyme from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+console.warn('RUNNING RUNNING RUNNING RUNNING RUNNING RUNNING RUNNING RUNNING RUNNING RUNNING RUNNING RUNNING RUNNING RUNNING RUNNING');
+
+Enzyme.configure({ adapter: new Adapter() });
+
+console.warn(Adapter);
