2c241b071957bea67d5c03e80ede7652c355db71
Adam Detrick <adetrick@cbinsights.com>
2019-01-09 17:40:29 -0500

    spruce up HOC to accept props

--- packages/fds-icons/scripts/templates/withFdsIconWrapper.jsx
+++ packages/fds-icons/scripts/templates/withFdsIconWrapper.jsx
@@ -1,11 +1,34 @@
 /* eslint-disable */
 import React from 'react';
+import cx from 'classnames';
+import PropTypes from 'prop-types';
 
-const withFdsIconWrapper = (WrappedComponent) => (props) => (
-  <div className={['fds-icon', props.className].join(' ')} style={{ fill: props.color }}>
-    <WrappedComponent />
-  </div>
-);
+const withFdsIconWrapper = (WrappedComponent) => (props) => {
+  const classNames = cx('fds-icon', `fds-icon--${props.size}`, props.className);
+
+  const otherProps = {};
+  if (props.color) {
+    otherProps.style = { fill: props.color };
+  }
+
+  const Component = (
+    <div className={classNames} {...otherProps}>
+      <WrappedComponent />
+    </div>
+  );
+
+  Component.defaultProps = {
+    size: 's',
+  };
+
+  Component.propTypes = {
+    color: PropTypes.string,
+    size: PropTypes.oneOf(['xs', 's', 'm', 'l', 'xl']),
+    className: PropTypes.string,
+  };
+
+  return Component;
+};
 
 withFdsIconWrapper.displayName = 'FdsIcon';
 
