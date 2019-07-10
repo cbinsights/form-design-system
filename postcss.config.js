28e33d0ccced4e4c2ffcf51a421a24b804af1ff4
Adam Detrick <adetrick@cbinsights.com>
2019-07-10 12:43:53 -0400

    postcss cleanup

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,18 @@
+/* eslint-disable */
+
 const plugins = require('./postcss.plugins');
 
-module.exports = ctx => ({
-  map: false,
-  plugins,
-});
+module.exports = (ctx) => {
+  const isProdBuild = Boolean(ctx.env === 'production');
+
+  const plugins = [...plugins];
+
+  if (isProdBuild) {
+    plugins.push(require('cssnano'));
+  }
+
+  return {
+    map: isProdBuild ? { inline: false } : false,
+    plugins,
+  };
+};
--- src/components/postcss.config.js
+++
@@ -1,8 +0,0 @@
-/* eslint-disable */
-
-const plugins = [...require('../../postcss.plugins'), require('cssnano')];
-
-module.exports = (ctx) => ({
-  map: false,
-  plugins,
-});
