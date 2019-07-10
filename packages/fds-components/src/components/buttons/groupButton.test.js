1f1732adc596797feac3d17026bcf16980043d99
Rey Wright <rey.wright@cbinsights.com>
2019-07-10 14:32:29 -0400

    remove unused variable in test, add cursor: default to groupbtn:disabled

--- packages/fds-components/src/components/buttons/groupButton.test.js
+++ packages/fds-components/src/components/buttons/groupButton.test.js
@@ -7,8 +7,6 @@ const Icon = () => (
   <span>🌭</span>
 )
 
-const button = { value: 'Feed', icon: Icon, isActive: true }
-
 describe('GroupButton component', () => {
   it('matches snapshot (set all props)', () => {
     expect(shallow(<GroupButton value='Feed' icon={Icon} isActive={true} />)).toMatchSnapshot();
