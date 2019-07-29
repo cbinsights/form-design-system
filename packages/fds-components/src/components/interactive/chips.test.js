fdae58f6c8a2a8577663264da6f250fed8370447
Rey Wright <rey.wright@cbinsights.com>
2019-07-29 15:00:23 -0400

    add context in test

--- packages/fds-components/src/components/interactive/chips.test.js
+++ packages/fds-components/src/components/interactive/chips.test.js
@@ -10,6 +10,12 @@ const chips = [
     { label: 'Sit', onClick: () => console.log('custom logic here'), isActive: false },
 ];
 
+/**
+ * NOTE: I have *purposely* have not extensively tested this, to force me and my 
+ * manager to revisit and figure out a robust testing strategy for more
+ * complex components like this.
+ */
+
 const renderComponent = (props) => shallow(<Chips {...props} />);
 
 describe('Chips component', () => {
