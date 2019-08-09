d3602c0d8c2edfc8d07071a839bfb38b645865c4
Rey Wright <rey.wright@cbinsights.com>
2019-08-08 20:34:20 -0400

    finish button group knobs

---
+++ src/components/interactive/buttonGroup.stories.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import { storiesOf } from '@storybook/react';
+import { withKnobs, object } from '@storybook/addon-knobs';
+
+import ButtonGroup from './ButtonGroup';
+import FeedIcon from '../../../lib/icons/react/FeedIcon';
+import '../style/index.css';
+
+const buttons = [
+  { label: 'Lorem', Icon: FeedIcon, isActive: true },
+  { label: 'Ipsum', Icon: FeedIcon, disabled: true },
+  { label: 'Dolor', Icon: FeedIcon },
+  { label: 'Sit', Icon: FeedIcon },
+];
+
+storiesOf('ButtonGroup', module)
+  .addDecorator(withKnobs)
+  .add('Knobs', () => <ButtonGroup buttons={object('buttons', buttons)} />);
