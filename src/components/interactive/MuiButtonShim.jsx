58bb82f5b0b9b68302e40bff74c892f1bc0557fb
Adam Detrick <adetrick@cbinsights.com>
2019-08-14 17:51:44 -0400

    hotfix mui button shim and menubutton

--- src/components/interactive/MuiButtonShim.jsx
+++ src/components/interactive/MuiButtonShim.jsx
@@ -13,7 +13,7 @@ import PropTypes from 'prop-types';
 const MuiButtonShim = ({ children, disabled, isRound, className, ...otherProps }) => (
   <button
     className={cx(
-      'fdsButton alignChild--center--center padding--all--half',
+      'fdsButton padding--all--half',
       {
         'fdsButton--round': isRound,
         'fdsButton--disabled': disabled,
