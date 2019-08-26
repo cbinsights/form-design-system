6f40d5f9e9ce1a0fd36d1e5ec128692d59bfd058
Rey Wright <rey.wright@cbinsights.com>
2019-08-26 16:41:46 -0400

    more buttons stuff

--- src/components/interactive/Button.jsx
+++ src/components/interactive/Button.jsx
@@ -1,18 +1,29 @@
 import React from 'react';
 import cx from 'classnames';
 
-const Button = ({ theme }) => (
+const Button = ({ theme, iconPlacement, Icon }) => (
   <button
     className={cx('niceButton', {
       'niceButton--primary': theme === 'primary--blue',
     })}
   >
-    Hello World
+    {Icon && iconPlacement === 'left' && (
+      <div className="margin--right--half niceButton-icon">
+        <Icon size="xs" />
+      </div>
+    )}
+    <div class="spinner"></div>
+    {Icon && iconPlacement === 'right' && (
+      <div className="margin--left--half niceButton-icon">
+        <Icon size="xs" />
+      </div>
+    )}
   </button>
 );
 
 Button.defaultProps = {
   theme: 'primary--blue',
+  iconPlacement: 'right',
 };
 
 export default Button;
--- src/components/interactive/button.stories.js
+++ src/components/interactive/button.stories.js
@@ -3,22 +3,35 @@ import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
 import { withKnobs, text, boolean, optionsKnob as options } from '@storybook/addon-knobs';
+import StarFilledIcon from '../../../lib/icons/react/StarFilledIcon';
 
 import Button from './Button';
 
 storiesOf('Interactive/Button', module)
   .addDecorator(withKnobs)
-  .add('Knobs', () => (
-    <Button
-      theme={options(
-        'theme',
-        {
-          blue: 'primary--blue',
-          red: 'primary--red',
-          orange: 'primary--orange',
-        },
-        'primary--blue',
-        { display: 'inline-radio' }
-      )}
-    />
-  ));
+  .add('Knobs', () => {
+    return (
+      <Button
+        Icon={boolean('Show Sample Icon?', false) && StarFilledIcon}
+        iconPlacement={options(
+          'iconPlacement',
+          {
+            left: 'left',
+            right: 'right',
+          },
+          'left',
+          { display: 'inline-radio' }
+        )}
+        theme={options(
+          'theme',
+          {
+            blue: 'primary--blue',
+            red: 'primary--red',
+            orange: 'primary--orange',
+          },
+          'primary--blue',
+          { display: 'inline-radio' }
+        )}
+      />
+    );
+  });
