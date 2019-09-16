fe46a84c631af8ccda03b88e0d7908068d143283
Rey Wright <rey.wright@cbinsights.com>
2019-09-16 18:05:33 -0400

    added small button snapshot test

---
+++ src/components/interactive/button.test.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+
+import Button from './Button';
+
+const renderComponent = (props) => shallow(<Button {...props} />);
+
+const Icon = () => (
+  <span>🌭</span>
+)
+
+const buttons = {
+  Link: () => {}, isLoading: true, isDestructive: true, disabled: true, iconPlacement: 'left',
+  Icon: () => {}, block: true, theme: 'outlined'
+};
+
+describe('ButtonGroup component', () => {
+
+  it('matches snapshot (default props)', () => {
+    const component = renderComponent({ children: 'Button' });
+    expect(component).toMatchSnapshot();
+  });
+
+  it('matches snapshot (set all props)', () => {
+    const component = renderComponent(buttons);
+    expect(component).toMatchSnapshot();
+  });
+});
