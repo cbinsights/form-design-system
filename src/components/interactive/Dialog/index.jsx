653001f55366a2f6e05ed7ae6677338e236ec9c5
Rey Wright <rey.wright@cbinsights.com>
2019-10-01 15:28:18 -0400

    adding ability to close via escape, start of CSSTransition

--- src/components/interactive/Dialog/index.jsx
+++ src/components/interactive/Dialog/index.jsx
@@ -1,36 +1,46 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import ReactDOM from 'react-dom';
+import { CSSTransition } from 'react-transition-group';
 import DenyIcon from '../../../../lib/icons/react/DenyIcon';
+
+const onKeyDown = (e) => e.keyCode === 27;
 
 const Dialog = (props) =>
   ReactDOM.createPortal(
-    <React.Fragment>
-      {props.isOpen && (
-        <React.Fragment>
-          <div className="dialog-overlay" onClick={props.onDismiss}></div>
-          <div className="dialog elevation--3" role="dialog">
-            {(props.title || props.canDismiss) && (
-              <React.Fragment>
-                <div className="dialog-header">
-                  <div className="dialog-title fontSize--xl">{props.title}</div>
-                  {props.canDismiss && (
-                    <div className="dialog-icon" onClick={props.onDismiss}>
-                      <DenyIcon size="xs" />
-                    </div>
-                  )}
-                </div>
-                <div className="dialog-divider" />
-              </React.Fragment>
-            )}
-            <div className="dialog-content">{props.content}</div>
-            {props.footerContent && (
-              <div className="dialog-footer">{props.footerContent}</div>
-            )}
-          </div>
-        </React.Fragment>
-      )}
-    </React.Fragment>,
+    <CSSTransition timeout={200}>
+      <React.Fragment>
+        {props.isOpen && (
+          <React.Fragment>
+            <div className="dialog-overlay" onClick={props.onDismiss}></div>
+            <div
+              className="dialog elevation--3"
+              role="dialog"
+              tabIndex="-1"
+              onKeyDown={(e) => onKeyDown(e) && props.onDismiss()}
+            >
+              {(props.title || props.canDismiss) && (
+                <React.Fragment>
+                  <div className="dialog-header">
+                    <div className="dialog-title fontSize--xl">{props.title}</div>
+                    {props.canDismiss && (
+                      <div className="dialog-icon" onClick={props.onDismiss}>
+                        <DenyIcon size="xs" />
+                      </div>
+                    )}
+                  </div>
+                  <div className="dialog-divider" />
+                </React.Fragment>
+              )}
+              <div className="dialog-content">{props.content}</div>
+              {props.footerContent && (
+                <div className="dialog-footer">{props.footerContent}</div>
+              )}
+            </div>
+          </React.Fragment>
+        )}
+      </React.Fragment>
+    </CSSTransition>,
     // eslint-disable-next-line no-undef
     document.body
   );
