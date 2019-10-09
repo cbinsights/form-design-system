8571f5fea79896419484d32bb4ce4cc399b601b3
Rey Wright <rey.wright@cbinsights.com>
2019-10-09 10:32:51 -0400

    adding test for modal closing on esc
    also removed usage of “simulate” as that is deprecated in enzyme

--- src/components/modals/dialog.test.js
+++ src/components/modals/dialog.test.js
@@ -7,7 +7,14 @@ describe('Dialog component', () => {
   it('dismisses modal when close icon is clicked', () => {
     const dismissFn = jest.fn()
     const wrapper = mount(<Dialog isOpen={true} onDismiss={dismissFn} content={<button>hey</button>} />);
-    wrapper.find('.dialog-icon').simulate('click');
+    wrapper.find('.dialog-icon').prop('onClick')();
+    expect(dismissFn).toHaveBeenCalled();
+  });
+
+  it('dismisses modal when esc is pressed', () => {
+    const dismissFn = jest.fn()
+    const wrapper = mount(<Dialog isOpen={true} onDismiss={dismissFn} content={<button>hey</button>} />);
+    wrapper.find('.dialog').prop('onKeyDown')({ key: 'Escape' })  
     expect(dismissFn).toHaveBeenCalled();
   });
 
