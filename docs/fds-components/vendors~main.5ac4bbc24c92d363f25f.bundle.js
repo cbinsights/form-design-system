283635c45bf15906bb6100cf932920c339fb43a7
Adam Detrick <adetrick@cbinsights.com>
2019-10-15 16:44:01 -0400

    clean up postcss plugins; fix syntax and scope errors with button and groupButton css

--- docs/fds-components/vendors~main.5ac4bbc24c92d363f25f.bundle.js
+++ docs/fds-components/vendors~main.5ac4bbc24c92d363f25f.bundle.js
@@ -103,4 +103,4 @@ function isObjectObject(o){return!0===
  * Released under the MIT License.
  */
 function isObject(val){return null!=val&&"object"==typeof val&&!1===Array.isArray(val)}(o)&&"[object Object]"===Object.prototype.toString.call(o)}function isPlainObject(o){var ctor,prot;return!1!==isObjectObject(o)&&("function"==typeof(ctor=o.constructor)&&(!1!==isObjectObject(prot=ctor.prototype)&&!1!==prot.hasOwnProperty("isPrototypeOf")))}__webpack_require__.r(__webpack_exports__),__webpack_require__.d(__webpack_exports__,"default",function(){return isPlainObject})}]]);
-//# sourceMappingURL=vendors~main.d1414d70b03b1f0fe58e.bundle.js.map
+//# sourceMappingURL=vendors~main.5ac4bbc24c92d363f25f.bundle.js.map
--- postcss.config.js
+++ postcss.config.js
@@ -3,8 +3,7 @@
 const plugins = [...require('./postcss.plugins')];
 
 module.exports = (ctx) => {
-  // const isProd = Boolean(ctx.env === 'production');
-  const isProd = false;
+  const isProd = Boolean(ctx.env === 'production');
   const isStylesDocs = Boolean(ctx.env === 'base_styles_docs');
 
   if (isProd) {
