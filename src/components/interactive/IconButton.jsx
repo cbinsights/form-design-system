580cdfa0e3daad7a97808c1e71bf1d9a1168eda9
Rey Wright <rey.wright@cbinsights.com>
2019-10-29 17:56:50 -0400

    added IconButton

---
+++ src/components/interactive/IconButton.jsx
@@ -0,0 +1,59 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import cx from 'classnames';
+
+const IconButton = ({
+  Icon,
+  disabled,
+  children,
+  isActive,
+  radius,
+  theme,
+  isLoading,
+  isDestructive,
+  ...rest
+}) => (
+  <button
+    {...rest}
+    className={cx('fdsIconButton', 'rounded--all', {
+      'fdsIconButton--disabled': disabled,
+      'fdsIconButton--active': isActive,
+      'fdsIconButton--circle': radius === 'circle',
+      'fdsIconButton--isDestructive': isDestructive,
+      'fdsIconButton--aqua': theme === 'aqua',
+      'fdsIconButton--ghost': theme === 'ghost',
+      'fdsIconButton--loading': isLoading,
+    })}
+    disabled={disabled}
+  >
+    <span className={isLoading ? 'fdsIconButton--hidden' : ''}>
+      <Icon size="xs" />
+    </span>
+  </button>
+);
+
+IconButton.defaultProps = {
+  radius: 'square',
+  theme: 'ghost',
+};
+
+IconButton.propTypes = {
+  /** Controls active style UI of button */
+  isActive: PropTypes.bool,
+  /** Controls radius of button (slightly rounded square, or circle) */
+  radius: PropTypes.oneOf(['square', 'circle']),
+  /** Controls spinner showing for icon button (normal icon is hidden) */
+  isLoading: PropTypes.bool,
+  /** Controls look and feel of button */
+  theme: PropTypes.oneOf('ghost', 'aqua'),
+  /** Controls destructive look and feel of button */
+  isDestructive: PropTypes.bool,
+  /** Controls whether the button is disabled or not. */
+  disabled: PropTypes.bool,
+  /** Used to render a FDS Icon (should only be used for FDS Icons) */
+  Icon: PropTypes.func.isRequred,
+  /** Contents inside the button */
+  children: PropTypes.node,
+};
+
+export default IconButton;
---
+++ src/components/interactive/iconButton.test.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+
+import IconButton from './IconButton';
+
+const renderComponent = (props) => shallow(<Button {...props} />);
+
+const buttons = {
+  Link: () => {}, isLoading: true, isDestructive: true, disabled: true, iconPlacement: 'left',
+  Icon: () => {}, isFullWidth: true, theme: 'outlined'
+};
+
+describe('ButtonGroup component', () => {
+
+  it('matches snapshot (default props)', () => {
+    const component = renderComponent({ children: 'Button' });
+    expect(component).toMatchSnapshot();
+  });
+
+  it('matches snapshot (set all props)', () => {
+    const component = renderComponent(buttons);
+    expect(component).toMatchSnapshot();
+  });
+});
