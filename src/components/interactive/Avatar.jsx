5f5256c434d410bc2d15231392651e568827ca5b
Rey Wright <wright.rey@gmail.com>
2019-12-10 17:33:27 -0500

    remove button functionality

--- src/components/interactive/Avatar.jsx
+++ src/components/interactive/Avatar.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import cx from 'classnames';
-import baseElement from '../../util/baseElement';
 
 export const VALID_BG_COLORS = [
   'white',
@@ -23,23 +22,20 @@ const grabInitials = (str) =>
     .splice(0, 2)
     .reduce((prev, curr) => prev + curr.charAt(0), '');
 
-const Avatar = ({ bgColor, imgUrl, size, name, Link, ...rest }) => {
-  const Element = baseElement({ href: rest.href, as: Link });
-  return (
-    <Element
-      {...rest}
-      title={name}
-      className={cx('fdsAvatar', `bgColor--${bgColor}`, `fdsAvatar--${size}`, {
-        'color--white': !NON_INVERTED_BG_COLORS.includes(bgColor),
-      })}
-    >
-      {imgUrl && (
-        <span className="fdsAvatar-img" style={{ backgroundImage: `url(${imgUrl})` }} />
-      )}
-      {name && grabInitials(name)}
-    </Element>
-  );
-};
+const Avatar = ({ bgColor, imgUrl, size, name, ...rest }) => (
+  <div
+    {...rest}
+    aria-title={name}
+    className={cx('fdsAvatar', `bgColor--${bgColor}`, `fdsAvatar--${size}`, {
+      'color--white': !NON_INVERTED_BG_COLORS.includes(bgColor),
+    })}
+  >
+    {imgUrl && (
+      <span className="fdsAvatar-img" style={{ backgroundImage: `url(${imgUrl})` }} />
+    )}
+    {name && grabInitials(name)}
+  </div>
+);
 
 Avatar.defaultProps = {
   bgColor: 'gray',
@@ -47,16 +43,6 @@ Avatar.defaultProps = {
 };
 
 Avatar.propTypes = {
-  /**
-   * Takes in a react-router `Link` reference and sets it
-   * as the base element. You may ONLY use it like the
-   * following:
-   * ```
-   * import { Link } from 'react-router'
-   * Link={Link}
-   * ```
-   */
-  Link: PropTypes.func,
   /** Controls color of button */
   bgColor: PropTypes.oneOf(VALID_BG_COLORS),
   /** Controls the size of the button */
