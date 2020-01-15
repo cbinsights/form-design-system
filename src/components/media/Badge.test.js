289a98aa5d18458a22a1ee03c63efb5393778f2e
Rey Wright <rey.wright@cbinsights.com>
2020-01-15 11:05:42 -0500

    add badge component

---
+++ src/components/media/Badge.test.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+
+import Badge from './Badge';
+
+describe('Badge component', () => {
+
+  it('matches snapshot (default props)', () => {
+    const component = shallow(<Badge label="badge" />);
+    expect(component).toMatchSnapshot();
+  });
+
+  it('matches snapshot (set all props)', () => {
+    const component = shallow(<Badge label="badge" size="xs" data-test="test" />)
+    expect(component).toMatchSnapshot();
+  });
+});
