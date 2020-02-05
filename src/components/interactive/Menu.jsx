f5adb7357f1544029faec5efb80c9edcffde9f11
Adam Detrick <adetrick@cbinsights.com>
2020-02-05 17:02:51 -0500

    WIP: using popover; callbacks returning whole JSX object

--- src/components/interactive/Menu.jsx
+++ src/components/interactive/Menu.jsx
@@ -3,49 +3,74 @@ import { useSelect } from 'downshift';
 import cx from 'classnames';
 import PropTypes from 'prop-types';
 
+import Popover from '../popovers/Popover';
+
 // TODO:
 // - [x] prop name cleanup
-// - [ ] custom trigger support
-// - [ ] popover
+// - [x] custom trigger support
+// - [x] popover
+// - [ ] selected state callbacks
 // - [ ] jsdoc it
 
-const Menu = ({ items, trigger }) => {
+const Menu = ({ items, trigger, onChange, disablePortal }) => {
   const {
     isOpen,
     getToggleButtonProps,
     getMenuProps,
     highlightedIndex,
     getItemProps,
-  } = useSelect({ items });
+  } = useSelect({
+    items,
+    onSelectedItemChange: (o) => onChange(o.selectedItem),
+  });
+
+  const clonedTrigger = React.cloneElement(trigger, getToggleButtonProps());
 
   return (
-    <div>
-      <div {...getToggleButtonProps()}>{trigger}</div>
-      <ul className={cx('fdsMenu', 'rounded--all', 'elevation--2')} {...getMenuProps()}>
-        {isOpen &&
-          items.map((item, index) => (
-            <li
-              className={cx('fdsMenu-item', {
-                'fdsMenu-item--active': highlightedIndex === index,
-              })}
-              key={`${item}${index}`}
-              {...getItemProps({ item, index })}
-            >
-              {item}
-            </li>
-          ))}
+    <Popover
+      trigger={clonedTrigger}
+      interactionMode="controlled"
+      isOpen={isOpen}
+      disablePortal={disablePortal}
+    >
+      <ul className="fdsMenu elevation--2 rounded--all" {...getMenuProps()}>
+        {items.map((item, index) => (
+          <li
+            className={cx('fdsMenu-item', {
+              'fdsMenu-item--active': highlightedIndex === index,
+            })}
+            key={`${item}${index}`}
+            {...getItemProps({ item, index })}
+          >
+            {item}
+          </li>
+        ))}
       </ul>
-    </div>
+    </Popover>
   );
 };
 
-Menu.defaultProps = {};
+Menu.defaultProps = {
+  disablePortal: false,
+  onChange: () => {},
+};
 
 Menu.propTypes = {
   /** JSX - Acts as a positioning reference for the popover and triggers active state */
   trigger: PropTypes.oneOfType([PropTypes.node, PropTypes.element]).isRequired,
 
+  /** Array of JSX items
+   *
+   * Example - mapping companies to JSX elements:
+   * `items={companies.map((c) => <div><strong>{c.name}</strong> {c.description}</div>)}`
+   */
   items: PropTypes.any,
+
+  /** disables portaling the popover to `document.body` */
+  disablePortal: PropTypes.bool,
+
+  /** change callback */
+  onChange: PropTypes.func,
 };
 
 export default Menu;
