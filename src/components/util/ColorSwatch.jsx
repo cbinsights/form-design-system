dc27a5990a6d70258f1b44e87f910f2f2437e2e6
Rey Wright <wright.rey@gmail.com>
2020-02-05 15:38:37 -0500

    Cleaned up FDS dictionary work

--- src/components/util/ColorSwatch.jsx
+++ src/components/util/ColorSwatch.jsx
@@ -1,104 +1,79 @@
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
-import { readableColor } from 'polished';
+import { mostReadable } from 'tinycolor2';
+import cx from 'classnames';
+
+const CopyButton = ({ value, position, copyToClipboard }) => (
+  <button
+    onClick={() => copyToClipboard(value)}
+    className={`swatchCopyButton swatchCopyButton--${position}`}
+  >
+    {value}
+  </button>
+);
+
+CopyButton.propTypes = {
+  value: PropTypes.string,
+  position: PropTypes.string,
+  copyToClipboard: PropTypes.func,
+};
+
+const mostReadableConfig = (hexName) =>
+  mostReadable(hexName, '#333', { includeFallbackColors: true, level: 'AAA' });
 
 export const ColorSwatch = (props) => {
   const [copiedText, setCopiedText] = useState('');
+  const [isHovered, setIsHovered] = useState(false);
 
   const copyToClipboard = (value) => {
+    // eslint-disable-next-line no-undef
     window.navigator.clipboard.writeText(value);
     setCopiedText(value);
     setTimeout(() => setCopiedText(''), 2000);
   };
 
-  const textClassName = `color--${props.jsVar.split('_')[1].toLowerCase()}`;
-  const bgClassName = `bgColor--${props.jsVar.split('_')[1].toLowerCase()}`;
   const colorName = props.jsVar.split('_')[1];
-  const hexName = props.hex;
+  const hexName = `#${props.hex.toUpperCase()}`;
+  const { cssVar, jsVar } = props;
+
+  const layouts = [
+    { value: jsVar, position: 'topRight' },
+    { value: cssVar, position: 'topLeft' },
+    { value: hexName, position: 'bottomRight' },
+  ];
 
   return (
-    <div className="elevation--1 display--inlineBlock swatch">
+    <div
+      className={cx('display--inlineBlock', 'rounded--all', 'swatch-container', {
+        'elevation--1': !isHovered,
+        'elevation--3': isHovered,
+      })}
+      onMouseEnter={() => setIsHovered(!isHovered)}
+      onMouseLeave={() => setIsHovered(!isHovered)}
+    >
       <div
-        style={{
-          backgroundColor: `#${hexName}`,
-          height: 100,
-          width: '100%',
-          borderRadius: 3,
-          position: 'relative',
-        }}
-        className="alignChild--center--center"
+        className="alignChild--center--center span--100 swatch"
+        style={{ backgroundColor: hexName, color: mostReadableConfig(hexName) }}
       >
-        <button
-          onClick={() => copyToClipboard(props.cssVar)}
-          style={{
-            top: '4px',
-            left: '4px',
-          }}
-        >
-          {props.cssVar}
-        </button>
-        <button
-          onClick={() => copyToClipboard(bgClassName)}
-          style={{
-            bottom: '4px',
-            left: '4px',
-          }}
-        >
-          {bgClassName}
-        </button>
-        <button
-          onClick={() => copyToClipboard(textClassName)}
-          style={{
-            bottom: '4px',
-            right: '4px',
-          }}
-        >
-          {textClassName}
-        </button>
-        <button
-          onClick={() => copyToClipboard(props.jsVar)}
-          style={{
-            top: '4px',
-            right: '4px',
-          }}
-        >
-          {props.jsVar}
-        </button>
-        {/* <div
-          style={{
-            backgroundColor: "#333",
-            color: "white",
-            position: "absolute",
-            top: 0,
-            right: 0,
-            fontSize: 10,
-            padding: "2px 6px",
-            borderBottomLeftRadius: 4
-          }}
-        >
-          #{props.hex.toUpperCase()}
-        </div> */}
-        <div
-          style={{ color: readableColor(`#${props.hex}`, '#333'), textAlign: 'center' }}
-        >
+        <div className="align--center">
           {!copiedText ? (
             <React.Fragment>
               <b>{colorName}</b>
-              <div
-                style={{
-                  color: readableColor(`#${props.hex}`, '#333'),
-                  fontSize: 10,
-                  opacity: 0.4,
-                }}
-              >
-                #{props.hex.toUpperCase()}
-              </div>
+              <div className="hex">{hexName}</div>
             </React.Fragment>
           ) : (
             <b>COPIED</b>
           )}
         </div>
-        {/* <div style={{ color: readableColor(`#${props.hex}`, '#333')}}><b>#{props.hex.toUpperCase()}</b></div> */}
+      </div>
+      <div className="content">
+        {layouts.map((layout) => (
+          <CopyButton
+            key={layout.value}
+            value={layout.value}
+            copyToClipboard={copyToClipboard}
+          />
+        ))}
       </div>
     </div>
   );
@@ -108,6 +83,14 @@ ColorSwatch.propTypes = {
   hex: PropTypes.string,
   cssVar: PropTypes.string,
   jsVar: PropTypes.string,
+};
+
+export const ColorSwatchWrapper = (props) => (
+  <div style={{ display: 'flex', flexWrap: 'wrap', margin: -6 }}>{props.children}</div>
+);
+
+ColorSwatchWrapper.propTypes = {
+  children: PropTypes.node,
 };
 
 export default ColorSwatch;
--- src/components/util/storybook.js
+++ src/components/util/storybook.js
@@ -1,11 +1,6 @@
-import React, { useState } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
-import { readableColor } from 'polished';
 import FDS from '../../../lib/dictionary/js/styleConstants';
-
-/**
- * Storybook helpers
- */
 
 /**
  * Creates a knobs options object where labels match values.
@@ -21,43 +16,6 @@ export const arrayToOptions = (values) =>
     }),
     {}
   );
-
-/**
- * @Object
- * useful background images for stories
- */
-export const storyBackgrounds = {
-  diagonalLine:
-    'url(data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAUAAAAFCAYAAACNbyblAAAAJ0lEQVQYV2NkQAP/b///z4gsBhZQZWSEC8IEQIrAgsgCYEF0AZAgAAvKE968p7/mAAAAAElFTkSuQmCC)',
-  check:
-    'url(data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAcAAAAHCAYAAADEUlfTAAAAHElEQVQYV2M8fPiwLwMOwAiStLW13YxNftBJAgAx2BqeI9XcBAAAAABJRU5ErkJggg==)',
-};
-
-export const StoryWrapper = ({ children }) => (
-  <div
-    className="display--inlineFlex"
-    style={{
-      flexWrap: 'wrap',
-      marginBottom: `-${FDS.SPACE_HALF}`,
-      marginRight: `-${FDS.SPACE_HALF}`,
-      alignItems: 'center',
-    }}
-  >
-    {children}
-  </div>
-);
-
-StoryWrapper.propTypes = {
-  children: PropTypes.node,
-};
-
-export const StoryItem = ({ children }) => (
-  <div className="margin--right--half margin--bottom--half">{children}</div>
-);
-
-StoryItem.propTypes = {
-  children: PropTypes.node,
-};
 
 /**
  * For displaying important info in MDX docs
@@ -90,87 +48,6 @@ export const Info = ({ type, children }) => {
   );
 };
 
-export const ColorSwatch = (props) => {
-  const [copiedText, setCopiedText] = useState('');
-
-  const copyToClipboard = (value) => {
-    window.navigator.clipboard.writeText(value);
-    setCopiedText(value);
-    setTimeout(() => setCopiedText(''), 2000);
-  };
-
-  return (
-    <div
-      className="elevation--1 display--inlineBlock"
-      style={{ margin: 6, borderRadius: 3, minWidth: 150, maxWidth: 300, flexGrow: 1 }}
-    >
-      <div
-        style={{
-          backgroundColor: `#${props.hex}`,
-          height: 100,
-          width: '100%',
-          borderTopLeftRadius: 3,
-          borderTopRightRadius: 3,
-          position: 'relative',
-        }}
-        className="alignChild--center--center"
-      >
-        {copiedText && <b>Copied</b>}
-        <div
-          style={{
-            backgroundColor: '#333',
-            color: 'white',
-            position: 'absolute',
-            top: 0,
-            right: 0,
-            fontSize: 12,
-            padding: '2px 6px',
-            borderBottomLeftRadius: 4,
-          }}
-        >
-          #{props.hex.toUpperCase()}
-        </div>
-        {/* <div style={{ backgroundColor: '#333', color: 'white', position: "absolute", top: 0, right: 0, fontSize: 12, padding: "2px 6px", borderBottomLeftRadius: 4, }}>
-          #{props.hex.toUpperCase()}
-        </div> */}
-        <div style={{ color: readableColor(`#${props.hex}`, '#333') }}>
-          <b>{props.jsVar.split('_')[1]}</b>
-        </div>
-        {/* <div style={{ color: readableColor(`#${props.hex}`, '#333')}}><b>#{props.hex.toUpperCase()}</b></div> */}
-      </div>
-      <div style={{ margin: '8px 8px', fontSize: 12 }}>
-        {!copiedText ? (
-          <React.Fragment>
-            <div onClick={() => copyToClipboard(props.cssVar)}>
-              <b>{props.cssVar}</b>
-            </div>
-            <div onClick={() => copyToClipboard(props.jsVar)}>{props.jsVar}</div>
-          </React.Fragment>
-        ) : (
-          <React.Fragment>
-            <p>Copied!</p>
-            <b>({copiedText})</b>
-          </React.Fragment>
-        )}
-      </div>
-    </div>
-  );
-};
-
-ColorSwatch.propTypes = {
-  hex: PropTypes.string,
-  cssVar: PropTypes.string,
-  jsVar: PropTypes.string,
-};
-
-export const ColorSwatchWrapper = (props) => (
-  <div style={{ display: 'flex', flexWrap: 'wrap', margin: -6 }}>{props.children}</div>
-);
-
-ColorSwatchWrapper.propTypes = {
-  children: PropTypes.node,
-};
-
 Info.defaultProps = {
   type: 'info',
 };
@@ -179,4 +56,39 @@ Info.propTypes = {
   children: PropTypes.oneOfType([PropTypes.arrayOf(PropTypes.node), PropTypes.node])
     .isRequired,
   type: PropTypes.oneOf(['warn', 'info']),
+};
+
+export default Info;
+
+export const StoryWrapper = ({ children }) => (
+  <div
+    className="display--inlineFlex"
+    style={{
+      flexWrap: 'wrap',
+      marginBottom: `-${FDS.SPACE_HALF}`,
+      marginRight: `-${FDS.SPACE_HALF}`,
+      alignItems: 'center',
+    }}
+  >
+    {children}
+  </div>
+);
+
+StoryWrapper.propTypes = {
+  children: PropTypes.node,
+};
+
+export const StoryItem = ({ children }) => (
+  <div className="margin--right--half margin--bottom--half">{children}</div>
+);
+
+StoryItem.propTypes = {
+  children: PropTypes.node,
+};
+
+export const storyBackgrounds = {
+  diagonalLine:
+    'url(data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAUAAAAFCAYAAACNbyblAAAAJ0lEQVQYV2NkQAP/b///z4gsBhZQZWSEC8IEQIrAgsgCYEF0AZAgAAvKE968p7/mAAAAAElFTkSuQmCC)',
+  check:
+    'url(data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAcAAAAHCAYAAADEUlfTAAAAHElEQVQYV2M8fPiwLwMOwAiStLW13YxNftBJAgAx2BqeI9XcBAAAAABJRU5ErkJggg==)',
 };
