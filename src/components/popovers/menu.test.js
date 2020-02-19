5f2788e5b8c63f8046f6e5ffa5dfb11736bad9d1
Adam Detrick <adetrick@cbinsights.com>
2020-02-19 10:23:29 -0500

    fix menu tests

--- src/components/popovers/menu.test.js
+++ src/components/popovers/menu.test.js
@@ -4,6 +4,12 @@ import { mount } from 'enzyme';
 import Menu from './Menu';
 import MenuItem from './MenuItem';
 
+// suppress CSS warnings
+jest.mock('@reach/utils', () => ({
+  ...jest.requireActual('@reach/utils'),
+  checkStyles: jest.fn(),
+}));
+
 describe('Popover component', () => {
 
   /**
@@ -12,20 +18,20 @@ describe('Popover component', () => {
    */
   it('matches snapshot (string items)', () => {
     const wrapper = mount(
-      <Menu trigger={<button>trigger</button>}>
-        <MenuItem>First</MenuItem>
-        <MenuItem>Second</MenuItem>
-        <MenuItem>Third</MenuItem>
+      <Menu trigger={<a href="#">trigger</a>}>
+        <MenuItem onSelect={() => {}}>First</MenuItem>
+        <MenuItem onSelect={() => {}}>Second</MenuItem>
+        <MenuItem onSelect={() => {}}>Third</MenuItem>
       </Menu>
     );
     expect(wrapper).toMatchSnapshot();
   });
   it('matches snapshot (JSX items)', () => {
     const wrapper = mount(
-      <Menu trigger={<button>trigger</button>}>
-        <MenuItem><p>First</p></MenuItem>
-        <MenuItem><p>Second</p></MenuItem>
-        <MenuItem><p>Third</p></MenuItem>
+      <Menu trigger={<a href="#">trigger</a>}>
+        <MenuItem onSelect={() => {}}><p>First</p></MenuItem>
+        <MenuItem onSelect={() => {}}><p>Second</p></MenuItem>
+        <MenuItem onSelect={() => {}}><p>Third</p></MenuItem>
       </Menu>
     );
     expect(wrapper).toMatchSnapshot();
