d8492de800718fa8b3e860ec53b61a3cb79b7f5d
John Nico <jnico@cbinsights.com>
2020-02-27 12:16:46 -0500

    fix lint

--- src/components/popovers/Menu.jsx
+++ src/components/popovers/Menu.jsx
@@ -6,25 +6,24 @@ import {
   MenuButton as ReachMenuButton,
 } from '@reach/menu-button';
 
-const Menu = ({ children, trigger }) => {
-  return (
-    <ReachMenu>
-      <ReachMenuButton className="resetButton border--focus rounded--all">
-        {trigger}
-      </ReachMenuButton>
-      <ReachMenuList className="elevation--2 rounded--all bgColor--white">
-        {children}
-      </ReachMenuList>
-    </ReachMenu>
-  );
-};
+const Menu = ({ children, trigger }) => (
+  <ReachMenu>
+    <ReachMenuButton className="resetButton border--focus rounded--all">
+      {trigger}
+    </ReachMenuButton>
+    <ReachMenuList className="elevation--2 rounded--all bgColor--white">
+      {children}
+    </ReachMenuList>
+  </ReachMenu>
+);
 
 Menu.propTypes = {
   /** JSX node - the element that should trigger the menu */
   trigger: PropTypes.node.isRequired,
 
   /** React children (should be of type `MenuItem`) */
-  children: PropTypes.oneOfType([PropTypes.arrayOf(PropTypes.node), PropTypes.node]).isRequired,
+  children: PropTypes.oneOfType([PropTypes.arrayOf(PropTypes.node), PropTypes.node])
+    .isRequired,
 };
 
 export default Menu;
