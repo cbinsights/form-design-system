ccf9554e2eb9c123208aaab9d892f893b8be2277
Rey Wright <rey.wright@cbinsights.com>
2020-02-27 19:05:15 -0500

    updated to 6.0 to fix rendering of image

--- .storybook/manager.js
+++ .storybook/manager.js
@@ -1,6 +1,8 @@
 import { addons } from '@storybook/addons';
 import { create } from '@storybook/theming/create';
 
+import logo from "/assets/FDS-logo.svg"
+
 const theme = create({
   brandTitle: 'Form Design System',
   brandUrl: 'cbinsights.github.io/form-design-system',
