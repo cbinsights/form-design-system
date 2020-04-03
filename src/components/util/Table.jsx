c5350d87b35af36c15794e87d61d632b689b59e0
Rey Wright <rey.wright@cbinsights.com>
2020-04-03 16:44:19 -0400

    Improved background documentation, completely refactored Table components
    - Removed DictionaryTableLayout, and replacing all instances being used with TableLayout component
    - Moved all specific cell logic applying to dictionary cells, to dictionary documentation
    - Increased copy element z-index
    - Move actual cell value into a different html element so that we can copy inner text directly
    - Moved all background examples into the table

--- src/components/util/Table.jsx
+++ src/components/util/Table.jsx
@@ -1,12 +1,8 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { mostReadable } from 'tinycolor2';
 import { useClipboard } from 'components/util/storybook';
 import ReactMarkdown from 'react-markdown';
 import cx from 'classnames';
-
-const camelToCaps = (camelCase) =>
-  camelCase.replace(/([A-Z])/g, (match) => ` ${match}`).toUpperCase();
 
 export const Table = (props) => (
   <table className={`doctable ${props.shrinkLastColumn ? 'shrinkLastColumn' : ''}`}>
@@ -19,45 +15,22 @@ Table.propTypes = {
   shrinkLastColumn: PropTypes.bool,
 };
 
-export const TableBody = (props) => <tbody>{props.children}</tbody>;
-
-TableBody.propTypes = {
-  children: PropTypes.node,
-};
-
-export const TableRow = (props) => <tr>{props.children}</tr>;
-
-TableRow.propTypes = {
-  children: PropTypes.node,
-};
-
-const mostReadableConfig = (hexName) =>
-  mostReadable(hexName, '#333', { includeFallbackColors: true, level: 'AAA' });
-
-export const TableCell = ({ cellType, children, label, isCSS, copy, ...props }) => {
+export const TableCell = ({ children, label, isCSS, copy, ...props }) => {
   const [copiedText, copyToClipboard] = useClipboard();
-
-  const newChildren =
-    typeof children === 'string' ? (
-      children.startsWith('rgba') ? (
-        <div style={{ background: children }}>{children}</div>
-      ) : (
-        <ReactMarkdown source={children} />
-      )
-    ) : (
-      children
-    );
-
   return (
     <td
       className={cx({
         hasCopy: copy,
         hasCSS: isCSS,
       })}
-      onClick={() => copyToClipboard(children)}
+      onClick={(e) => {
+        copyToClipboard(e.currentTarget.firstElementChild.innerText.trim());
+      }}
       {...props}
     >
-      {newChildren}
+      <div style={{ width: '100%' }}>
+        {typeof children === 'string' ? <ReactMarkdown source={children} /> : children}
+      </div>
       {copy && (
         <span className="cliptext">
           {copiedText ? <b>Copied to Clipboard</b> : 'Copy to Clipboard'}
@@ -70,12 +43,11 @@ export const TableCell = ({ cellType, children, label, isCSS, copy, ...props }) 
 TableCell.propTypes = {
   children: PropTypes.node,
   copy: PropTypes.bool,
-  cellType: PropTypes.string,
   isCSS: PropTypes.bool,
   label: PropTypes.string,
 };
 
-export const TableHeadLayout = ({ headers }) => (
+const TableHeadLayout = ({ headers }) => (
   <thead>
     <tr>
       {headers.map((header) => (
@@ -89,34 +61,10 @@ TableHeadLayout.propTypes = {
   headers: PropTypes.array,
 };
 
-export const DictionaryTableLayout = ({ data, copy = true }) => (
-  <Table shrinkLastColumn>
-    <TableHeadLayout
-      headers={Object.keys(data[0]).map((header) => camelToCaps(header))}
-    />
-    <TableBody>
-      {data.map((row, idx) => (
-        <tr key={idx}>
-          <TableCell copy={copy}>{row.cssVar}</TableCell>
-          <TableCell copy={copy}>{row.jsVar}</TableCell>
-          <TableCell copy={copy} cellType={row.jsVar}>
-            {row.value}
-          </TableCell>
-        </tr>
-      ))}
-    </TableBody>
-  </Table>
-);
-
-DictionaryTableLayout.propTypes = {
-  data: PropTypes.any,
-  copy: PropTypes.bool,
-};
-
 export const TableLayout = ({ headers, shrinkLastColumn, rows, isCSS, copy = true }) => (
   <Table shrinkLastColumn={shrinkLastColumn}>
     <TableHeadLayout headers={headers} />
-    <TableBody>
+    <tbody>
       {rows.map((row, idx) => (
         <tr key={idx}>
           {row.map((cell, idx2) =>
@@ -130,7 +78,7 @@ export const TableLayout = ({ headers, shrinkLastColumn, rows, isCSS, copy = tru
           )}
         </tr>
       ))}
-    </TableBody>
+    </tbody>
   </Table>
 );
 
