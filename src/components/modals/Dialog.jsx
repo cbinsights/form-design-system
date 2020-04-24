59dba7f9bb9827ef5f8bd30651b1d2657d3662a8
Rey Wright <rey.wright@cbinsights.com>
2020-04-24 18:11:19 -0400

    reach modal
    new stuff
    
    removed height
    
    add height 100% to html & body
    
    update reach menu button

--- src/components/modals/Dialog.jsx
+++ src/components/modals/Dialog.jsx
@@ -2,12 +2,12 @@ import React, { useEffect, useLayoutEffect, useRef, useState } from 'react';
 import PropTypes from 'prop-types';
 import ReactDOM from 'react-dom';
 import { CSSTransition } from 'react-transition-group';
-import FocusTrap from 'focus-trap-react';
 import noScroll from 'no-scroll';
 import rafSchd from 'raf-schd';
 import DenyIcon from 'lib/icons/react/DenyIcon';
 import IconButton from 'components/interactive/IconButton';
 import Section from 'components/layout/Section';
+import { Dialog as ReachDialog } from '@reach/dialog';
 
 export const isElementOverflowing = ({ current }) => {
   // Checking for current first is safer just in case,
@@ -58,84 +58,62 @@ const Dialog = ({
     return undefined;
   }, [alwaysShowBorder]);
 
-  useLayoutEffect(() => {
-    // This toggles scrolling on and off based on whether the modal
-    // is shown or not
-    if (isOpen) {
-      noScroll.on();
-    } else {
-      noScroll.off();
-    }
-  }, [isOpen]);
-
   const dialogNode = (
-    <div>
-      <div className="dialog-overlay"></div>
-      <div className="dialog-zIndex dialog-wrapper">
-        <div
-          className="dialog elevation--3"
-          role={role}
-          aria-labelledby={title && 'a11y-dialog-title'}
-          aria-describedby="a11y-dialog-desc"
-          tabIndex="-1"
-          aria-modal="true"
-          onKeyDown={handleKeyDown}
-          style={{
-            maxWidth: `${width}${typeof width === 'number' ? 'px' : ''} `,
-            maxHeight: `${height}${typeof height === 'number' ? 'px' : ''}`,
-          }}
-        >
-          {(title || onDismiss) && (
-            <React.Fragment>
-              <div className="dialog-header">
-                <Section border="bottom">
-                  <div className="padding--right--double type--head4">
-                    {title ? <span id="a11y-dialog-title">{title}</span> : '\u00A0'}{' '}
-                    {/* There always needs to be something (even a space) in the header for display reasons */}
-                  </div>
-                  {onDismiss && (
-                    <div className="dialog-icon">
-                      <IconButton
-                        Icon={DenyIcon}
-                        onClick={onDismiss}
-                        aria-label="close"
-                        label="close"
-                      />
-                    </div>
-                  )}
-                </Section>
+    <ReachDialog
+      className="dialog elevation--3"
+      role={role}
+      aria-labelledby={title && 'a11y-dialog-title'}
+      aria-describedby="a11y-dialog-desc"
+      aria-modal="true"
+      onKeyDown={handleKeyDown}
+      style={{
+        maxWidth: `${width}${typeof width === 'number' ? 'px' : ''} `,
+        maxHeight: `${height}${typeof height === 'number' ? 'px' : ''}`,
+      }}
+    >
+      {(title || onDismiss) && (
+        <React.Fragment>
+          <div className="dialog-header">
+            <Section border="bottom">
+              <div className="padding--right--double type--head4">
+                {title ? <span id="a11y-dialog-title">{title}</span> : '\u00A0'}{' '}
+                {/* There always needs to be something (even a space) in the header for display reasons */}
               </div>
-            </React.Fragment>
-          )}
-          <div className="dialog-content" ref={contentEl}>
-            <Section>{content}</Section>
+              {onDismiss && (
+                <div className="dialog-icon">
+                  <IconButton
+                    Icon={DenyIcon}
+                    onClick={onDismiss}
+                    aria-label="close"
+                    label="close"
+                  />
+                </div>
+              )}
+            </Section>
           </div>
-          {footerContent && (
-            <div className="dialog-footer">
-              <Section
-                border={alwaysShowBorder || isOverflowing ? 'top' : undefined}
-                bgColor="white"
-              >
-                {footerContent}
-              </Section>
-            </div>
-          )}
-        </div>
+        </React.Fragment>
+      )}
+      <div className="dialog-content" ref={contentEl}>
+        <Section>{content}</Section>
       </div>
-    </div>
+      {footerContent && (
+        <div className="dialog-footer">
+          <Section
+            border={alwaysShowBorder || isOverflowing ? 'top' : undefined}
+            bgColor="white"
+          >
+            {footerContent}
+          </Section>
+        </div>
+      )}
+    </ReachDialog>
   );
 
   return (
     <React.Fragment>
-      {ReactDOM.createPortal(
-        <CSSTransition timeout={200} in={isOpen} classNames="dialog" unmountOnExit>
-          <React.Fragment>
-            {disableFocusTrap ? dialogNode : <FocusTrap>{dialogNode}</FocusTrap>}
-          </React.Fragment>
-        </CSSTransition>,
-        // eslint-disable-next-line no-undef
-        document.body
-      )}
+      <CSSTransition timeout={200} in={isOpen} classNames="dialog" unmountOnExit>
+        <React.Fragment>{dialogNode}</React.Fragment>
+      </CSSTransition>
     </React.Fragment>
   );
 };
