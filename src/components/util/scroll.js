9e2f1da133b7934c62de69ab6a653083f94ccd85
Rey Wright <rey.wright@cbinsights.com>
2020-04-28 11:35:45 -0400

    remove noscroll package. Replced with exact copy, with modifications so that it does not add top

---
+++ src/components/util/scroll.js
@@ -0,0 +1,69 @@
+// Adapted from the following:
+// https://github.com/davidtheclark/no-scroll/blob/master/index.js
+/* eslint-disable */
+
+(function(root) {
+  let isOn = false;
+  let scrollbarSize;
+  let scrollTop;
+
+  function getScrollbarSize() {
+    if (typeof scrollbarSize !== 'undefined') return scrollbarSize;
+
+    const doc = document.documentElement;
+    const dummyScroller = document.createElement('div');
+    dummyScroller.setAttribute(
+      'style',
+      'width:99px;height:99px;' + 'position:absolute;top:-9999px;overflow:scroll;'
+    );
+    doc.appendChild(dummyScroller);
+    scrollbarSize = dummyScroller.offsetWidth - dummyScroller.clientWidth;
+    doc.removeChild(dummyScroller);
+    return scrollbarSize;
+  }
+
+  function hasScrollbar() {
+    return document.documentElement.scrollHeight > window.innerHeight;
+  }
+
+  function on(options) {
+    if (typeof document === 'undefined' || isOn) return;
+    const doc = document.documentElement;
+    if (hasScrollbar()) {
+      doc.style.width = `calc(100% - ${getScrollbarSize()}px)`;
+    } else {
+      doc.style.width = '100%';
+    }
+    doc.style.overflow = 'hidden';
+    isOn = true;
+  }
+
+  function off() {
+    if (typeof document === 'undefined' || !isOn) return;
+    const doc = document.documentElement;
+    doc.style.width = '';
+    doc.style.overflow = '';
+    window.scroll(0, scrollTop);
+    isOn = false;
+  }
+
+  function toggle() {
+    if (isOn) {
+      off();
+      return;
+    }
+    on();
+  }
+
+  const noScroll = {
+    on,
+    off,
+    toggle,
+  };
+
+  if (typeof module !== 'undefined' && typeof module.exports !== 'undefined') {
+    module.exports = noScroll;
+  } else {
+    root.noScroll = noScroll;
+  }
+})(this);
