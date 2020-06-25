a54b3437b55811933084bc00a54ce53af075ac3b
Rey Wright <wright.rey@gmail.com>
2020-06-25 17:32:26 -0400

    update selectors to be more specific

--- src/components/Button/button.test.js
+++ src/components/Button/button.test.js
@@ -8,7 +8,7 @@ describe('Button component', () => {
 
   it('tests default rendering', () => {
     render(<Button label="Button" />);
-    const button = screen.getByRole('button');
+    const button = screen.getByRole('button', { name: "Button" });
     expect(button).toBeTruthy();
     expect(button).toHaveClass('fdsButton--m');
     expect(button).toHaveClass('fdsButton--blue');
@@ -17,8 +17,8 @@ describe('Button component', () => {
   });
 
   it('tests rendering as link', () => {
-    render(<Button label="Button" href="#" />);
-    const link = screen.getByRole('link');
+    render(<Button label="Anchor" href="#" />);
+    const link = screen.getByRole('link', { name: "Anchor" });
     expect(link).toBeTruthy();
   });
 
@@ -30,7 +30,7 @@ describe('Button component', () => {
 
   it('tests that proper theme gets applied', () => {
     render(<Button label="Button" theme="outlined" />);
-    const button = screen.getByRole('button');
+    const button = screen.getByRole('button', { name: "Button"});
     expect(button).toHaveClass('fdsButton--outlined');
     expect(button).not.toHaveClass('fdsButton--blue');
   });
@@ -44,7 +44,7 @@ describe('Button component', () => {
         isFullWidth	
       />
     );
-    const button = screen.getByRole('button');
+    const button = screen.getByRole('button', { name: "Button" });
     expect(button).toHaveClass('fdsButton--disabled');
     expect(button).toHaveClass('fdsButton--loading');
     expect(button).toHaveClass('fdsButton--isFullWidth');
