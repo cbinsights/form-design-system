407a22d6e47d0c5f88cacd460ea25e981f7efe42
Rey Wright <wright.rey@gmail.com>
2020-07-07 18:11:59 -0400

    add babylon parser to command

--- scripts/bash/transforms/transform.sh
+++ scripts/bash/transforms/transform.sh
@@ -2,4 +2,7 @@
 #!/bin/sh
 # [ $(git rev-parse HEAD) = $(git ls-remote $(git rev-parse --abbrev-ref @{u} | \
 # sed 's/\// /g') | cut -f1) ] && echo up to date || echo not up to date
-jscodeshift --verbose 1 --extensions js,jsx -t $@
+
+# Usage: 
+
+jscodeshift --verbose 1 --extensions js,jsx -t $@ --parser babylon
