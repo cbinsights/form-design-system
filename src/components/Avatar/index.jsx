647ac3b6720af63694d488fb4d5fcf9e9348f2a2
Rey Wright <wright.rey@gmail.com>
2020-07-23 19:52:50 -0400

    Enhancements to Avatar
    - Add size large to avatar
     - Add PlaceholderIcon to Avatar
     - migrated tests over to react-testing-library

--- src/components/Avatar/index.jsx
+++ src/components/Avatar/index.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 import PropTypes from 'prop-types';
 import cc from 'classcat';
 import baseElement from 'util/baseElement';
@@ -7,7 +7,7 @@ const LIGHT_COLORS = ['white', 'haze', 'lightGray'];
 const DARK_COLORS = ['orange', 'charcoal', 'navy', 'aqua', 'gray', 'red', 'purple'];
 export const RADII = ['square', 'circle'];
 export const BG_COLORS = [...LIGHT_COLORS, ...DARK_COLORS];
-export const SIZES = ['s', 'm'];
+export const SIZES = ['s', 'm', 'l'];
 export const INITIALS_LENGTH = [1, 2];
 
 export const grabInitials = (str, initialsCount = 2) =>
@@ -26,21 +26,37 @@ const Avatar = ({
   imgUrl,
   name,
   Link,
+  PlaceholderIcon,
   ...rest
 }) => {
   const Element = baseElement({ href: rest.href, onClick: rest.onClick, as: Link });
+
+  const placeholderIconSize = useCallback(() => {
+    switch (size) {
+      case 's':
+        return 16;
+      case 'm':
+        return 18;
+      case 'l':
+        return 24;
+      default:
+        return 16;
+    }
+  }, [size]);
+
   return (
     <Element
       {...rest}
       role={Element === 'div' ? 'img' : undefined}
       aria-label={ariaLabel}
-      title={name}
+      title={name || 'Placeholder Avatar'}
       className={cc([
         {
           'border--focus': Element !== 'div',
           'color--white': DARK_COLORS.includes(bgColor),
         },
         'fdsAvatar',
+        'alignChild--center--center',
         `bgColor--${bgColor}`,
         `fdsAvatar--${size}`,
         `fdsAvatar--${radius}`,
@@ -50,6 +66,9 @@ const Avatar = ({
         <span className="fdsAvatar-img" style={{ backgroundImage: `url(${imgUrl})` }} />
       )}
       {name && grabInitials(name, initialsLength)}
+      {!(name || imgUrl) && PlaceholderIcon && (
+        <PlaceholderIcon customSize={placeholderIconSize()} />
+      )}
     </Element>
   );
 };
@@ -62,11 +81,13 @@ Avatar.propTypes = {
   /** Controls radius of button (slightly rounded square, or circle) */
   radius: PropTypes.oneOf(RADII),
   /** Control initials displayed, and also used for title accessibility attribute (uses 2 words max for initials) */
-  name: PropTypes.string.isRequired,
+  name: PropTypes.string,
   /** Sets background image over initials */
   imgUrl: PropTypes.string,
   /** Controls how many intials can be displayed */
   initialsLength: PropTypes.oneOf(INITIALS_LENGTH),
+  /**  Accepts FDS Icon. Renders if name or imgUrl are not present  */
+  PlaceholderIcon: PropTypes.func,
   /**
    * Takes in a react-router `Link` reference and sets it
    * as the base element. You may ONLY use it like the
