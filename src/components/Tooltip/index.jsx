60c3d54fd3554d00b4de93c8444603213cf3b8a8
Kevin Chen <kevin.chen@cbinsights.com>
2020-09-18 16:39:13 -0400

    UIP-936: Add position prop to Tooltip

--- src/components/Tooltip/index.jsx
+++ src/components/Tooltip/index.jsx
@@ -1,24 +1,25 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import FDS from 'lib/dictionary/js/styleConstants';
-import Popover from 'components/Popover';
+import Popover, { VALID_POSITIONS } from 'components/Popover';
 
 export const DEFAULT_WIDTH = '240px';
 export const DELAY_MS = 350;
+export const DEFAULT_POSITION = 'bottom';
 
-const Tooltip = ({ trigger, message, maxWidth }) => (
+const Tooltip = ({ trigger, message, maxWidth, position = DEFAULT_POSITION }) => (
   <Popover
     interactionMode="hover"
     trigger={<span style={{ cursor: 'help' }}>{trigger}</span>}
     distance={parseInt(FDS.SPACE_HALF, 10)}
-    position="bottom"
+    position={position}
     alignment="center"
     transitionName="GrowFast"
     delay={DELAY_MS}
   >
     <div
       role="tooltip"
-      style={{ maxWidth: maxWidth ? `${maxWidth}px` : '240px' }}
+      style={{ maxWidth: maxWidth ? `${maxWidth}px` : DEFAULT_WIDTH }}
       className="bgColor--charcoal inverted align--center padding--top--half padding--bottom--half padding--left padding--right elevation--2 rounded--all fontSize--s fontWeight--bold"
     >
       {message}
@@ -35,6 +36,12 @@ Tooltip.propTypes = {
 
   /** Maximum width of tooltip */
   maxWidth: PropTypes.number,
+
+  /**
+   * Position preference of tooltip.
+   * `top` for example, will place the tooltip above the trigger.
+   */
+  position: PropTypes.oneOf(VALID_POSITIONS),
 };
 
 export default Tooltip;
