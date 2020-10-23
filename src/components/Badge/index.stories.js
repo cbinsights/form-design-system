1d8dd771c91f13e96032f01021a77c8bc05e2d60
Rey Wright <wright.rey@gmail.com>
2020-10-23 11:11:04 -0400

    convert Badge and Button group documentation to CSF

---
+++ src/components/Badge/index.stories.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Badge from '.';
+
+const Template = (args) => <Badge {...args} />;
+
+export const Primary = Template.bind({});
+Primary.args = {
+  label: 'Badge',
+};
+
+export default {
+  component: Badge,
+  title: 'components/Badge',
+  parameters: {
+    docs: {
+      description: {},
+    },
+  },
+};
---
+++ src/components/ButtonGroup/index.stories.js
@@ -0,0 +1,80 @@
+import React from 'react';
+import Updatable from 'components/Updatable';
+import Tooltip from 'components/Tooltip';
+import FeedIcon from 'lib/icons/react/FeedIcon';
+import State from 'util/storybook-docs/State';
+import ButtonGroup from '.';
+
+const Template = (args) => <ButtonGroup {...args} />;
+
+export const Primary = Template.bind({});
+Primary.args = {
+  buttons: [
+    { label: 'Lorem', disabled: true },
+    { label: 'Ipsum' },
+    { label: 'Dolor', isActive: true },
+  ],
+};
+
+export const ActiveStates = () => (
+  <State
+    initialValue={1}
+    render={(value, setValue) => (
+      <ButtonGroup
+        buttons={['Lorem', 'Sit', 'Ipsum'].map((label, idx) => ({
+          label,
+          onClick: () => setValue(idx),
+          isActive: value === idx,
+        }))}
+      />
+    )}
+  />
+);
+
+export const Wrapper = Template.bind({});
+Wrapper.args = {
+  buttons: [
+    {
+      label: 'Updatable',
+      // eslint-disable-next-line
+      Wrapper: (props) => <Updatable message="42">{props.children}</Updatable>,
+    },
+    {
+      label: 'Tooltip',
+      // eslint-disable-next-line
+      Wrapper: (props) => (
+        // eslint-disable-next-line
+        <Tooltip trigger={props.children} message="hey" maxWidth={240} />
+      ),
+    },
+  ],
+};
+
+Wrapper.parameters = {
+  docs: {
+    description: {
+      story: 'Allows you to render a component around any of the individual buttons',
+    },
+  },
+};
+
+export const Icons = Template.bind({});
+Icons.args = {
+  buttons: [
+    { label: 'Lorem', Icon: FeedIcon },
+    { label: 'Sit', Icon: FeedIcon },
+  ],
+};
+
+Icons.parameters = {
+  docs: {
+    description: {
+      story: 'Displays on the left',
+    },
+  },
+};
+
+export default {
+  component: ButtonGroup,
+  title: 'components/ButtonGroup',
+};
