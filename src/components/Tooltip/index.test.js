a4f8d66225191754ff5ba037feba1baf7f321e57
Rey Wright <wright.rey@gmail.com>
2020-10-30 13:59:53 -0400

    migrated tooltip to use Tippy

--- src/components/Tooltip/index.test.js
+++ src/components/Tooltip/index.test.js
@@ -1,9 +1,8 @@
 import React from 'react';
-import { mount, shallow } from 'enzyme';
+import { mount } from 'enzyme';
 
-import Tooltip, { DEFAULT_WIDTH } from '.';
+import Tooltip from '.';
 
-const SELECTOR_TOOLTIP = '[role="tooltip"]';
 
 const Trigger = () => (<p>trigger</p>);
 
@@ -12,29 +11,5 @@ describe('Tooltip component', () => {
     const wrapper = mount(<Tooltip trigger={<Trigger />} message="lol" />);
     expect(wrapper).toMatchSnapshot();
   })
-
-  it('sets the right default width', () => {
-    const tooltipMaxWidth = shallow(
-      <Tooltip
-        trigger={<Trigger />}
-        message="tooltip with default max width"
-      />
-    ).find(SELECTOR_TOOLTIP).prop('style').maxWidth;
-
-    expect(tooltipMaxWidth).toBe(DEFAULT_WIDTH);
-  });
-
-  it('overrides default width with `maxWidth` is passed in', () => {
-    const evil = 666;
-    const tooltipMaxWidth = shallow(
-      <Tooltip
-        trigger={<Trigger />}
-        message="tooltip with evil max width"
-        maxWidth={evil}
-      />
-    ).find(SELECTOR_TOOLTIP).prop('style').maxWidth;
-
-    expect(tooltipMaxWidth).toBe(`${evil}px`);
-  });
 
 });
