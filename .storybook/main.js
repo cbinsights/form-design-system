47ee25ecaba6b1105b1186aac748966cd273139e
Rey Wright <wright.rey@gmail.com>
2020-11-04 15:31:50 -0500

    resume picking up mdx files from docs folders

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 module.exports = {
-  stories: ["../src/**/*.stories.@(js|mdx)"],
+  stories: ["../(src|docs)/**/*.stories.@(js|mdx)"],
   addons: [
     "@storybook/addon-essentials",
     "@storybook/addon-a11y"
