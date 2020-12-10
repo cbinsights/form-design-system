fc2ba9e6992538b178e80c8fbf6a788b8975f8ae
Rey Wright <wright.rey@gmail.com>
2020-12-10 11:36:27 -0600

    tippy

--- src/components/Tooltip/index.jsx
+++ src/components/Tooltip/index.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import FDS from 'lib/dictionary/js/styleConstants';
 import Tippy from '@tippyjs/react';
-import { VALID_POSITIONS } from 'components/Popover';
 
 export const DEFAULT_WIDTH = 240;
 export const DELAY_MS = 350;
@@ -10,24 +9,23 @@ export const DEFAULT_POSITION = 'bottom';
 export const VALID_TEXT_ALIGN = ['left', 'center', 'right'];
 
 const Tooltip = ({
-  triggerRef,
   position = DEFAULT_POSITION,
   trigger,
   message,
   maxWidth = DEFAULT_WIDTH,
-  interactive,
+  textAlign = 'center',
 }) => (
   <Tippy
     arrow={false}
-    interactive={interactive}
+    theme="tooltip"
     delay={[DELAY_MS, 0]}
-    reference={triggerRef}
     maxWidth={maxWidth}
     content={message}
     placement={position}
     zIndex={FDS.ZINDEX_MODAL}
     moveTransition="transform 0.2s ease-out"
     appendTo={document.body}
+    className={`bgColor--charcoal inverted align--${textAlign} padding--top--half padding--bottom--half padding--left padding--right elevation--2 rounded--all fontSize--s fontWeight--bold`}
   >
     {trigger}
   </Tippy>
@@ -37,25 +35,30 @@ Tooltip.propTypes = {
   /** JSX - Acts as a positioning reference for the popover and triggers active state */
   trigger: PropTypes.oneOfType([PropTypes.node, PropTypes.element]),
 
-  triggerRef: PropTypes.oneOfType([
-    // Either a function
-    PropTypes.func,
-    // Or the instance of a DOM native element (see the note about SSR)
-    PropTypes.shape({ current: PropTypes.any }),
-  ]),
-
   /** Content of the tooltip */
-  message: PropTypes.node.isRequired,
+  message: PropTypes.string.isRequired,
 
   /** Maximum width of tooltip */
   maxWidth: PropTypes.number,
 
-  /**
-   * Position preference of tooltip.
-   * `top` for example, will place the tooltip above the trigger.
-   */
-  position: PropTypes.oneOf(VALID_POSITIONS),
-  interactive: PropTypes.bool,
+  /** Position preference of tooltip. */
+  position: PropTypes.oneOf([
+    'top-start',
+    'top-end',
+    'right',
+    'right-start',
+    'right-end',
+    'bottom',
+    'bottom-start',
+    'bottom-end',
+    'left',
+    'left-start',
+    'auto',
+    'auto-start',
+    'auto-end',
+  ]),
+  /** Text align for the message */
+  textAlign: PropTypes.oneOf(['left', 'center', 'right']),
 };
 
 export default Tooltip;
