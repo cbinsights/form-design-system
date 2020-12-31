03d7fe94cfa6baab281fbe1742af4698d18d0d50
Rey Wright <wright.rey@gmail.com>
2020-12-31 11:32:53 -0500

    put helper function in a different file

--- src/components/Popover/index.jsx
+++ src/components/Popover/index.jsx
@@ -3,18 +3,7 @@ import PropTypes from 'prop-types';
 import Tippy from '@tippyjs/react';
 import FDS from 'lib/dictionary/js/styleConstants';
 import hideOnEscFunc from './hideOnEsc';
-
-const triggerHelper = (trigger, isOpen) => {
-  // https://atomiks.github.io/tippyjs/v6/all-props/#trigger
-  // We'd like hover to semantically map to mouseenter + focus
-  const triggerMapping = {
-    click: 'click',
-    hover: 'mouseenter focus',
-  };
-  /* Tippy will throw a warning if you attempt to pass a trigger when
-     isOpen is passed. This will avoid showing that warning. */
-  return isOpen !== undefined ? undefined : triggerMapping[trigger];
-};
+import triggerHelper from './triggerHelper';
 
 const Popover = ({
   children,
---
+++ src/components/Popover/triggerHelper.js
@@ -0,0 +1,13 @@
+const triggerHelper = (trigger, isOpen) => {
+  // https://atomiks.github.io/tippyjs/v6/all-props/#trigger
+  // We'd like hover to semantically map to mouseenter + focus
+  const triggerMapping = {
+    click: 'click',
+    hover: 'mouseenter focus',
+  };
+  /* Tippy will throw a warning if you attempt to pass a trigger when
+     isOpen is passed. This will avoid showing that warning. */
+  return isOpen !== undefined ? undefined : triggerMapping[trigger];
+};
+
+export default triggerHelper;
