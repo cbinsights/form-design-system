c86c5bad24c9e4496443a7e0fd9eb1e25effda43
Rey Wright <wright.rey@gmail.com>
2021-02-22 11:59:37 -0600

    changed tooltip test so that it tests actual functionality

--- src/components/Tooltip/index.test.js
+++ src/components/Tooltip/index.test.js
@@ -1,13 +1,15 @@
 import React from 'react';
-import { mount } from 'enzyme';
-
+import { render, waitFor, screen } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import userEvent from '@testing-library/user-event';
 import Tooltip from '.';
 
-const Trigger = () => <p>trigger</p>;
-
 describe('Tooltip component', () => {
-  it('matches snapshot (uses the right classes)', () => {
-    const wrapper = mount(<Tooltip trigger={<Trigger />} message="lol" />);
-    expect(wrapper).toMatchSnapshot();
+  it('tests that message gets rendered on hover', async () => {
+    render(<Tooltip trigger={<button>trigger</button>} message="lol" />);
+    userEvent.hover(screen.getByText('trigger'));
+    await waitFor(() => {
+      expect(screen.getByText('lol')).toBeTruthy();
+    });
   });
 });
