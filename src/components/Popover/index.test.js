5688bf9efc8a3d25e962d6e259113d2b31dd1d2e
Rey Wright <wright.rey@gmail.com>
2021-02-22 11:58:59 -0600

    need to await for tests using LazyTippy now

--- src/components/Popover/index.test.js
+++ src/components/Popover/index.test.js
@@ -1,27 +1,27 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render, waitFor, screen } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 
 import Popover from '.';
 
 describe('Popover component', () => {
-  it('tests that trigger text gets rendered', () => {
+  it('tests that trigger text gets rendered', async () => {
     render(
       <Popover triggerElement={<button>trigger</button>}>
         <p>popover content</p>
       </Popover>
     );
     expect(screen.queryByText('popover content')).toBeNull();
-    expect(screen.getByText('trigger')).toBeTruthy();
+    await waitFor(() => expect(screen.getByText('trigger')).toBeTruthy());
   });
 
-  it('tests that trigger and content text gets rendered', () => {
+  it('tests that trigger and content text gets rendered', async () => {
     render(
       <Popover isOpen triggerElement={<button>trigger</button>}>
         <p>popover content</p>
       </Popover>
     );
-    expect(screen.getByText('popover content')).toBeTruthy();
     expect(screen.getByText('trigger')).toBeTruthy();
+    await waitFor(() => expect(screen.getByText('popover content')).toBeTruthy());
   });
 });
