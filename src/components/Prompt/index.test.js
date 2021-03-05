3c3aefd1b2d071555ac658c572e03dc2b9e89ff8
Rey Wright <wright.rey@gmail.com>
2021-03-05 03:05:01 -0600

    simple rtl tests for Prompt

--- src/components/Prompt/index.test.js
+++ src/components/Prompt/index.test.js
@@ -1,37 +1,19 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { render, screen } from '@testing-library/react';
 import { Button } from 'components';
 import Prompt from '.';
 
 describe('Prompt component', () => {
-  it('matches snapshot (default props)', () => {
-    const component = shallow(
-      <Prompt
-        primaryButton={<Button label="foo" />}
-        secondaryButton={<Button label="bar" />}
-      />
-    );
-    expect(component).toMatchSnapshot();
-    const component2 = shallow(
+  it('tests that prompt renders properly', () => {
+    render(
       <Prompt
         isOpen
         primaryButton={<Button label="foo" />}
         secondaryButton={<Button label="bar" />}
       />
     );
-    expect(component2).toMatchSnapshot();
-  });
 
-  it('matches snapshot (set all props)', () => {
-    const component = shallow(
-      <Prompt
-        primaryButton={<Button label="foo" />}
-        secondaryButton={<Button label="bar" />}
-        title="title"
-        desc="desc"
-        isOpen
-      />
-    );
-    expect(component).toMatchSnapshot();
+    expect(screen.getByText('foo')).toBeTruthy();
+    expect(screen.getByText('bar')).toBeTruthy();
   });
 });
