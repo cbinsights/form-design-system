5904389a7f06215f73b7a30abc57937f8b1ac86a
Rey Wright <wright.rey@gmail.com>
2021-05-25 10:58:43 -0500

    update storybook, resolve implicit postcss deprecation

--- .storybook/main.js
+++ .storybook/main.js
@@ -2,32 +2,8 @@ const path = require('path');
 
 module.exports = {
   stories: ['../src/**/*.stories.@(js|mdx)', '../guidelines/**/*.stories.@(js|mdx)'],
-  addons: ['@storybook/addon-essentials', '@storybook/addon-a11y'],
-  webpackFinal: async (config, { configType }) => {
-    // Rey wrote this, blame him.
-    // Essentially, we want to keep all the rest of the
-    // rules present, add our own css rule, and ignore the existing css rule
-    // that's in here
-    newConfig = config.module.rules.filter((rule) => {
-      return String(rule.test) !== '/\\.css$/';
-    });
-    config.module.rules = newConfig;
-    config.module.rules.push({
-      test: /\.css$/,
-      include: path.resolve(__dirname, '../'),
-      use: [
-        'style-loader',
-        {
-          loader: 'postcss-loader',
-          options: {
-            config: {
-              path: './',
-            },
-          },
-        },
-      ],
-    });
-
+  addons: ['@storybook/addon-postcss', '@storybook/addon-essentials', '@storybook/addon-a11y'],
+  webpackFinal: async (config) => {
     config.resolve.modules = [...(config.resolve.modules || []), path.resolve('./src')];
 
     config.resolve.alias = {
