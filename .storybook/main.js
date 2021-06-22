c86e681748dfe287707afe2febe77ec826bfda1f
Rey Wright <wright.rey@gmail.com>
2021-06-22 15:24:29 -0500

    add postcss to remove implicit dependency which will be removed in v7 of storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 module.exports = {
   stories: ['../src/**/*.stories.@(js|ts|mdx|tsx)', '../guidelines/**/*.stories.@(js|ts|mdx|tsx)'],
-  addons: ['@storybook/addon-essentials', '@storybook/addon-a11y'],
+  addons: ['@storybook/addon-postcss', '@storybook/addon-essentials', '@storybook/addon-a11y'],
   webpackFinal: async (config) => {
     config.resolve.modules = [...(config.resolve.modules || []), path.resolve('./src')];
 
