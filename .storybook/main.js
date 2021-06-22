52c9d64a3bbb05b5446349f4fc7ec7bc3edf0047
Rey Wright <wright.rey@gmail.com>
2021-06-22 15:04:33 -0500

    remove configType

--- .storybook/main.js
+++ .storybook/main.js
@@ -3,7 +3,7 @@ const path = require('path');
 module.exports = {
   stories: ['../src/**/*.stories.@(js|ts|mdx|tsx)', '../guidelines/**/*.stories.@(js|ts|mdx|tsx)'],
   addons: ['@storybook/addon-essentials', '@storybook/addon-a11y'],
-  webpackFinal: async (config, { configType }) => {
+  webpackFinal: async (config) => {
     config.resolve.modules = [...(config.resolve.modules || []), path.resolve('./src')];
 
     config.resolve.alias = {
