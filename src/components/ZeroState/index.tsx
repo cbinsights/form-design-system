78988363af7cb5f45cd4d7eb3f74fb21f08494b8
Rey Wright <wright.rey@gmail.com>
2021-11-05 13:48:48 -0500

    migrating ZeroState component to TypeScript

--- src/components/ZeroState/index.tsx
+++ src/components/ZeroState/index.tsx
@@ -1,8 +1,14 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import FDS from 'lib/dictionary/js/styleConstants';
+import Icon from 'components/Icon';
 
-const ZeroState = ({ Icon, label = '', height = '0px' }) => (
+export type Props = {
+  Icon?: Icon;
+  label?: string | number | React.ReactNode;
+  height?: string;
+};
+
+const ZeroState = ({ Icon, label = '', height = '0px' }: Props): JSX.Element => (
   <article className="alignChild--center--center" style={{ minHeight: height }}>
     <section>
       {Icon && (
@@ -14,22 +20,5 @@ const ZeroState = ({ Icon, label = '', height = '0px' }) => (
     </section>
   </article>
 );
-
-ZeroState.propTypes = {
-  /**
-   * Pass in "only" a FDS Icon reference to display it (e.g. Icon={TableIcon})
-   */
-  Icon: PropTypes.func,
-
-  /**
-   * Text inside
-   */
-  label: PropTypes.oneOfType([PropTypes.string, PropTypes.number, PropTypes.node]),
-
-  /**
-   * Min height of section
-   */
-  height: PropTypes.string,
-};
 
 export default ZeroState;
