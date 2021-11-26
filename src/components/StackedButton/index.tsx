8eb64cbe8d97a3ba86a91e1438fd174257a27599
Iván Hernández <ivan.hernandez@cbinsights.com>
2021-11-26 15:18:43 -0700

    fix: migrate StackedButton to typescript.

--- src/components/StackedButton/index.tsx
+++ src/components/StackedButton/index.tsx
@@ -1,11 +1,55 @@
 import React, { forwardRef } from 'react';
-import PropTypes from 'prop-types';
 import cc from 'classcat';
 import baseElement from 'util/baseElement';
 import CaretDownIcon from 'icons/react/CaretDownIcon';
+import { IconProps } from 'components/Icon';
+
+export interface StackedButtonProps {
+  /**
+   * Takes in a react-router `Link` reference and sets it
+   * as the base element. You may ONLY use it like the
+   * following:
+   *
+   * - `import { Link } from 'react-router'`
+   * - `Link={Link}`
+   */
+  Link: React.ElementType;
+  /**
+   * Controls whether the button is disabled or not. Will control the disabled
+   * presentation of either an anchor or button rendered under the hood,
+   * but will only add a disabled attribute for buttons
+   */
+  disabled?: boolean;
+  /**  Pass in "only" a FDS Icon reference to display it (e.g. Icon={ApproveIcon}) */
+  Icon: React.ComponentType<IconProps>;
+  /** Text inside the button */
+  label: string | number;
+  /** Controls the active state, which changes UI (colors) */
+  isActive?: boolean;
+  /** Controls the toggled state, which changes UI (colors) */
+  isToggled?: boolean;
+  /** Controls whether caret icon is displayed */
+  hasCaret?: boolean;
+  /** URL for anchor element */
+  href?: string;
+  /** handler for onclick event */
+  onClick?: () => void;
+}
 
 const StackedButton = forwardRef(
-  ({ Icon, Link, label, disabled, isActive, isToggled, hasCaret, ...rest }, ref) => {
+  (
+    {
+      Icon,
+      Link,
+      label,
+      disabled,
+      isActive,
+      isToggled,
+      hasCaret,
+      ...rest
+    }: StackedButtonProps,
+    ref
+  ) => {
     const Element = baseElement({ href: rest.href, onClick: true, as: Link });
 
     const ariaPressed = () => {
@@ -47,33 +91,5 @@ const StackedButton = forwardRef(
 );
 
 StackedButton.displayName = 'StackedButton';
-
-StackedButton.propTypes = {
-  /**
-   * Takes in a react-router `Link` reference and sets it
-   * as the base element. You may ONLY use it like the
-   * following:
-   *
-   * - `import { Link } from 'react-router'`
-   * - `Link={Link}`
-   */
-  Link: PropTypes.elementType,
-  /**
-   * Controls whether the button is disabled or not. Will control the disabled
-   * presentation of either an anchor or button rendered under the hood,
-   * but will only add a disabled attribute for buttons
-   */
-  disabled: PropTypes.bool,
-  /**  Pass in "only" a FDS Icon reference to display it (e.g. Icon={ApproveIcon}) */
-  Icon: PropTypes.func,
-  /** Text inside the button */
-  label: PropTypes.oneOfType([PropTypes.string, PropTypes.number]).isRequired,
-  /** Controls the active state, which changes UI (colors) */
-  isActive: PropTypes.bool,
-  /** Controls the toggled state, which changes UI (colors) */
-  isToggled: PropTypes.bool,
-  /** Controls whether caret icon is displayed */
-  hasCaret: PropTypes.bool,
-};
 
 export default StackedButton;
