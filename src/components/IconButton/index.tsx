18daef594a65ad84017ac7e2c9259ed0823234ef
Iván Hernández <ivan.hernandez@cbinsights.com>
2021-11-26 19:20:14 -0700

    fix: migrate IconButton to typescript.

--- src/components/IconButton/index.tsx
+++ src/components/IconButton/index.tsx
@@ -1,11 +1,45 @@
 import React, { forwardRef } from 'react';
-import PropTypes from 'prop-types';
 import cc from 'classcat';
 import baseElement from 'util/baseElement';
+import { IconProps } from 'components/Icon';
 
 export const THEMES = ['ghost', 'aqua', 'outlined'];
 export const RADII = ['square', 'circle'];
 export const SIZES = ['s', 'm'];
+
+export interface IconButtonProps {
+  /**
+   * Takes in a react-router `Link` reference and sets it
+   * as the base element. You may ONLY use it like the
+   * following:
+   *
+   * - `import { Link } from 'react-router'`
+   * - `Link={Link}`
+   */
+  Link?: React.ElementType;
+  /** Controls active style UI of button */
+  isActive?: boolean;
+  /** Controls radius of button (slightly rounded square, or circle) */
+  radius?: 'square' | 'circle';
+  /** Controls spinner showing for icon button (normal icon is hidden) */
+  isLoading?: boolean;
+  /** Controls look and feel of button */
+  theme?: 'ghost' | 'aqua' | 'outlined';
+  /** Controls destructive look and feel of button */
+  isDestructive?: boolean;
+  /** Used to control the size of the button */
+  size?: 's' | 'm';
+  /** Controls whether the button is disabled or not. */
+  disabled?: boolean;
+  /** Used to render a FDS Icon (should only be used for FDS Icons) */
+  Icon: React.ComponentType<IconProps>;
+  /** Accessibility label */
+  label: string;
+  /** Extend click radius of button to nearest relative parent */
+  isBreakoutLink: boolean;
+  /** URL for base element */
+  href: string;
+}
 
 const IconButton = forwardRef(
   (
@@ -22,7 +56,7 @@ const IconButton = forwardRef(
       Link,
       isBreakoutLink,
       ...rest
-    },
+    }: IconButtonProps,
     ref
   ) => {
     const Element = baseElement({ href: rest.href, onClick: true, as: Link });
@@ -62,37 +96,5 @@ const IconButton = forwardRef(
 );
 
 IconButton.displayName = 'IconButton';
-
-IconButton.propTypes = {
-  /**
-   * Takes in a react-router `Link` reference and sets it
-   * as the base element. You may ONLY use it like the
-   * following:
-   *
-   * - `import { Link } from 'react-router'`
-   * - `Link={Link}`
-   */
-  Link: PropTypes.elementType,
-  /** Controls active style UI of button */
-  isActive: PropTypes.bool,
-  /** Controls radius of button (slightly rounded square, or circle) */
-  radius: PropTypes.oneOf(RADII),
-  /** Controls spinner showing for icon button (normal icon is hidden) */
-  isLoading: PropTypes.bool,
-  /** Controls look and feel of button */
-  theme: PropTypes.oneOf(THEMES),
-  /** Controls destructive look and feel of button */
-  isDestructive: PropTypes.bool,
-  /** Used to control the size of the button */
-  size: PropTypes.oneOf(SIZES),
-  /** Controls whether the button is disabled or not. */
-  disabled: PropTypes.bool,
-  /** Used to render a FDS Icon (should only be used for FDS Icons) */
-  Icon: PropTypes.func.isRequired,
-  /** Accessibility label */
-  label: PropTypes.string.isRequired,
-  /** Extend click radius of button to nearest relative parent */
-  isBreakoutLink: PropTypes.bool,
-};
 
 export default IconButton;
