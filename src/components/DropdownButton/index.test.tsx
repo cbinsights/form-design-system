05e6aa3df6020229f2a73cada7777187482e0c82
Iván Hernández <ivan.hernandez@cbinsights.com>
2021-11-30 14:41:17 -0700

    use `screen`instead of helpers for unit tesing

--- src/components/DropdownButton/index.test.tsx
+++ src/components/DropdownButton/index.test.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { screen, render } from '@testing-library/react';
 import DropdownButton from '.';
 
 describe('DropdownButton component', () => {
   it('renders correctly', () => {
-    const { getByText } = render(<DropdownButton>Dropdown</DropdownButton>);
-    const child = getByText('Dropdown');
+    render(<DropdownButton>Dropdown</DropdownButton>);
+    const child = screen.getByText('Dropdown');
     expect(child).toBeDefined();
   });
 });
