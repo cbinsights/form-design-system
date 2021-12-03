aa59737f07a52227866f72d7695d90537fe5e3e6
Rey Wright <wright.rey@gmail.com>
2021-12-03 12:07:28 -0600

    build(yarn): fix tests

--- jest.config.js
+++ jest.config.js
@@ -5,4 +5,5 @@ module.exports = {
   testPathIgnorePatterns: ['<rootDir>/lib/'],
   coveragePathIgnorePatterns: ['<rootDir>/src/icons/react'],
   setupFilesAfterEnv: ['<rootDir>/scripts/node/setupTests.js', 'jest-prop-type-error'],
+  snapshotSerializers: ['enzyme-to-json/serializer'],
 };
