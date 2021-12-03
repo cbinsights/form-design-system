79da32aea62c87f0db1e5f57e053aec211bb8d1c
Rey Wright <wright.rey@gmail.com>
2021-12-03 11:12:15 -0600

    build(yarn): change to plug n play

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,8 @@
 module.exports = {
   testEnvironment: 'jest-environment-jsdom',
-  moduleDirectories: ['node_modules', 'src'],
+  moduleDirectories: ['src'],
   rootDir: './',
-  testPathIgnorePatterns: ['<rootDir>/node_modules/', '<rootDir>/lib/'],
+  testPathIgnorePatterns: ['<rootDir>/lib/'],
   coveragePathIgnorePatterns: ['<rootDir>/src/icons/react'],
   setupFilesAfterEnv: ['<rootDir>/scripts/node/setupTests.js', 'jest-prop-type-error'],
-  snapshotSerializers: ['<rootDir>/node_modules/enzyme-to-json/serializer'],
 };
