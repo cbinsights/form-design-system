37363bcc88ad49e6c409b661d0001b63521b849c
Rey Wright <wright.rey@gmail.com>
2021-12-03 21:57:54 -0600

    chore(build): improve strictness of eslint (#767)
    fixes #768

--- .eslintrc.js
+++ .eslintrc.js
@@ -34,6 +34,11 @@ module.exports = {
       },
     ],
   },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
   overrides: [
     {
       // enable the rule specifically for TypeScript files
