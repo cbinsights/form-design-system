5fa42f851423d27b78ed460fa1f5095dd0bdc70b
David Cohen <dovidcohen35@gmail.com>
2021-12-13 12:51:12 -0500

    fix(typescript): convert InputGroup to TypeScript
    fixes #788

--- src/components/InputGroup/index.tsx
+++ src/components/InputGroup/index.tsx
@@ -3,6 +3,8 @@ import React from 'react';
 import Flex from 'components/Flex';
 import FlexItem from 'components/FlexItem';
 
+type FlexType = 'shrink' | 'grow'[];
+
 /**
  * Returns an array InputGroup can use to set grow/shrink on flex items
  *
@@ -10,7 +12,7 @@ import FlexItem from 'components/FlexItem';
  * @param {Array} userSettings - `flexSettings` prop optionally set by user
  * @returns {Array} e.g. ['shrink', 'grow', 'shrink']
  */
-export const getFlexSettings = (childCount: number, userSettings: string[]): string[] => {
+export const getFlexSettings = (childCount: number, userSettings: FlexType): FlexType => {
   // always obey a valid `flexSettings` prop when passed by a user
   if (userSettings && userSettings.length === childCount) return userSettings;
 
@@ -26,25 +28,11 @@ export const getFlexSettings = (childCount: number, userSettings: string[]): str
   return defaultSettings;
 };
 
-export interface InputGroupProps {
-  /** `input` or `select` elements */
-  children: React.ReactNode;
-
-  /**
-   * Controls how the input segments should grow or shrink to fill remaining space.
-   *
-   * `shrink` - segment to shrink to natural width of the input element
-   *
-   * `grow` - grow the input element to fill remaining space
-   */
-  flexSettings?: 'shrink' | 'grow';
-}
-
 /**
  * @param {Object} props react props
  * @returns {ReactElement}
  */
-const InputGroup = ({ children, flexSettings }: InputGroupProps): JSX.Element => {
+const InputGroup = ({ children, flexSettings }: InputGroupProps): React.ReactNode => {
   const growShrinkArr = getFlexSettings(React.Children.count(children), flexSettings);
 
   return (
@@ -57,5 +45,19 @@ const InputGroup = ({ children, flexSettings }: InputGroupProps): JSX.Element =>
     </div>
   );
 };
+
+export interface InputGroupProps {
+  /** `input` or `select` elements */
+  children: React.ReactNode;
+
+  /**
+   * Controls how the input segments should grow or shrink to fill remaining space.
+   *
+   * `shrink` - segment to shrink to natural width of the input element
+   *
+   * `grow` - grow the input element to fill remaining space
+   */
+  flexSettings: FlexType;
+}
 
 export default InputGroup;
