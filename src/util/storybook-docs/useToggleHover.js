ce0c42364427a87949b83fa0378e229b541b794d
Iván Hernández <ivan.hernandez@cbinsights.com>
2021-12-15 14:23:32 -0600

    fix(storybook): migrate storybook helpers to typescript

--- src/util/storybook-docs/useToggleHover.js
+++
@@ -1,7 +0,0 @@
-import { useState } from 'react';
-
-export default () => {
-  const [isHovered, setIsHovered] = useState(false);
-  const toggleHover = () => setIsHovered(!isHovered);
-  return [isHovered, toggleHover];
-};
