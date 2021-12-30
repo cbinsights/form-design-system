a0aecb0439719555a936b118127d07f112b71a51
David Cohen <59481400+David-Cohen35@users.noreply.github.com>
2021-12-30 11:38:30 -0500

    chore(deps): remove d3-scale and its associated functions (#935)
    fixes #932

--- scripts/node/dictionary/util/color.js
+++ scripts/node/dictionary/util/color.js
@@ -1,5 +1,4 @@
 const Color = require('tinycolor2');
-const d3 = require('d3-scale');
 
 /**
  * For whatever reason, style-dictionary uses high decimal precision
@@ -14,27 +13,6 @@ exports.toHsla = (hsl) => {
   const A = parseInt(hsl.a, 10);
   return `hsla(${H},${S},${L},${A})`;
 };
-
-/**
- * Get a value from a range based on color brightness
- *
- * @param {String} color hex color string
- * @param {Array} r range to map to color brightness (0-255)
- * @param {string} scaleType optional scale type
- * @returns {Number}
- */
-const brightScale = (color, r, scaleType = 'scaleLinear') =>
-  d3[scaleType]().domain([0, 255]).range(r)(Color(color).getBrightness());
-
-/**
- * Get a value from a range based on color saturation
- *
- * @param {String} color hex color string
- * @param {Array} r range to map to color saturation (0-1)
- * @returns {Number}
- */
-const satScale = (color, r) =>
-  d3.scaleLinear().domain([0, 1]).range(r)(Color(color).toHsl().s);
 
 /**
  * @param {String} base color from which get R,G,B components
