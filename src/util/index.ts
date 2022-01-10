f875d4b80767428e1075af729fcd5543a328fd21
Iván Hernández <ivan.hernandez@cbinsights.com>
2022-01-10 15:03:05 -0600

    refactor(test): remove 'getKeys' and refactor test cases to be done with static values

--- src/util/index.ts
+++ src/util/index.ts
@@ -1,5 +1,1 @@
 export const noop = (..._args: unknown[]): void => {};
-/**
- * method returns an array of a given object's own enumerable property names, iterated in the same order that a normal loop would, with its types
- */
-export const getKeys = Object.keys as <T extends object>(obj: T) => Array<keyof T>;
