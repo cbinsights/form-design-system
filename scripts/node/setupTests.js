0be9f0fcad1bf544830f7dcf8cfbad67245514e7
David Cohen <dovidcohen35@gmail.com>
2022-01-18 13:08:20 -0500

    refactor(test): remove enzyme dependencies
    fixes #996

--- scripts/node/setupTests.js
+++ scripts/node/setupTests.js
@@ -1,9 +1,4 @@
-/* eslint-disable */
-import Enzyme from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
 import '@testing-library/jest-dom/extend-expect';
-
-Enzyme.configure({ adapter: new Adapter() });
 
 // Shimming window.scroll for 3rd party libraries
 window.scroll = () => {};
