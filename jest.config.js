73a3f3fc35dc77a0cc809e0284d5fed34590568c
David Cohen <59481400+David-Cohen35@users.noreply.github.com>
2022-01-18 16:29:17 -0500

    refactor(test): remove enzyme dependencies (#997)
    * refactor(test): remove enzyme dependencies
    
    fixes #996
    
    * refactor(test): remove enzyme deps
    
    fixes #996

--- jest.config.js
+++ jest.config.js
@@ -5,6 +5,5 @@ module.exports = {
   testPathIgnorePatterns: ['<rootDir>/lib/'],
   coveragePathIgnorePatterns: ['<rootDir>/src/icons/react'],
   setupFilesAfterEnv: ['<rootDir>/scripts/node/setupTests.js', 'jest-prop-type-error'],
-  snapshotSerializers: ['enzyme-to-json/serializer'],
   testRegex: '(/__tests__/.*|(\\.|/)(test))\\.[jt]sx?$',
 };
