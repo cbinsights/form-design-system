6d700bfb399d6e35dcb269602e2ef7665b9deaa7
David Cohen <59481400+David-Cohen35@users.noreply.github.com>
2022-03-08 10:11:57 -0500

    fix(tooltip): fixes tooltip getting stuck in place and migrates to radix ui (#1074)
    fixes #1002
    
    Co-authored-by: David Cohen <david.cohen@cbinsights.com>

--- scripts/node/setupTests.js
+++ scripts/node/setupTests.js
@@ -2,3 +2,17 @@ import '@testing-library/jest-dom/extend-expect';
 
 // Shimming window.scroll for 3rd party libraries
 window.scroll = () => {};
+
+global.ResizeObserver = class ResizeObserver {
+  constructor(cb) {
+    this.cb = cb;
+  }
+  observe() {
+    this.cb([{ borderBoxSize: { inlineSize: 0, blockSize: 0 } }]);
+  }
+  unobserve() {}
+};
+
+global.DOMRect = {
+  fromRect: () => ({ top: 0, left: 0, bottom: 0, right: 0, width: 0, height: 0 }),
+};
