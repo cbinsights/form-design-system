8af9068152a754fd7ffd23fcbe29797326863003
David Cohen <david.cohen@cbinsights.com>
2022-03-09 13:31:05 -0500

    add playwright files

---
+++ playwright/example.spec.ts
@@ -0,0 +1,20 @@
+// example.spec.ts
+import { test, expect } from '@playwright/test';
+
+// eslint-disable-next-line jest/no-done-callback
+test('my test', async ({ page }) => {
+  await page.goto('https://playwright.dev/');
+
+  // Expect a title "to contain" a substring.
+  await expect(page).toHaveTitle(/Playwright/);
+
+  // Expect an attribute "to be strictly equal" to the value.
+  await expect(page.locator('text=Get Started').first()).toHaveAttribute(
+    'href',
+    '/docs/intro'
+  );
+
+  await page.click('text=Get Started');
+  // Expect some text to be visible on the page.
+  await expect(page.locator('text=Introduction').first()).toBeVisible();
+});
