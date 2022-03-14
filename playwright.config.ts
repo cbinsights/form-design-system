dac22b368f3020e3328ab78bca751ea749cf59f9
David Cohen <david.cohen@cbinsights.com>
2022-03-14 17:31:23 -0400

    ci(playwright): removes safari and firefox from config

--- playwright.config.ts
+++ playwright.config.ts
@@ -12,14 +12,6 @@ const config: PlaywrightTestConfig = {
       name: 'chromium',
       use: { ...devices['Desktop Chrome'] },
     },
-    {
-      name: 'firefox',
-      use: { ...devices['Desktop Firefox'] },
-    },
-    {
-      name: 'webkit',
-      use: { ...devices['Desktop Safari'] },
-    },
   ],
 };
 export default config;
