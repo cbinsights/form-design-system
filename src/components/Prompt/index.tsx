c55f9e696f34a24f9b1dd2f29beb5169df20b42f
David Cohen <david.cohen@cbinsights.com>
2022-03-15 15:30:28 -0400

    refactor(prompt): migrate prompt to radix ui AlertDialog

--- src/components/Prompt/index.tsx
+++ src/components/Prompt/index.tsx
@@ -1,8 +1,23 @@
-import React from 'react';
-import Dialog from 'components/Dialog';
+import React, { useEffect, useRef, useState } from 'react';
+import cc from 'classcat';
 import Flex from 'components/Flex';
+import { DenyIcon } from 'icons/react';
+import { IconButton } from 'components';
 import FlexItem from 'components/FlexItem';
 import { ButtonProps } from 'components/Button';
+import * as AlertDialog from '@radix-ui/react-alert-dialog';
+import rafSchd from 'raf-schd';
+
+export const isElementOverflowing = ({
+  current,
+}: React.MutableRefObject<HTMLDivElement | null>): boolean => {
+  // Checking for current first is safer just in case,
+  // and will also fail gracefully in tests
+  if (current) {
+    return current.scrollHeight > current.clientHeight;
+  }
+  return false;
+};
 
 export interface PromptProps {
   /** Controls text in header */
@@ -24,21 +39,103 @@ export interface PromptProps {
   disablePortal?: boolean;
 }
 
-const Prompt = (props: PromptProps): JSX.Element => (
-  <Dialog
-    content={props.desc ? <div id="a11y-dialog-desc">{props.desc}</div> : null}
-    footerContent={
-      <Flex justify="end">
-        {props.secondaryButton && <FlexItem shrink>{props.secondaryButton}</FlexItem>}
-        {props.primaryButton && <FlexItem shrink>{props.primaryButton}</FlexItem>}
-      </Flex>
-    }
-    role="alertdialog"
-    title={props.title}
-    isOpen={props.isOpen}
-    onDismiss={props.onDismiss}
-    disablePortal={props.disablePortal}
-  />
-);
+const Prompt = ({
+  title,
+  desc,
+  primaryButton,
+  secondaryButton,
+  isOpen,
+  onDismiss,
+}: PromptProps): JSX.Element => {
+  const contentEl = useRef<HTMLDivElement>(null);
+  const [isOverflowing, setIsOverflowing] = useState(false);
+  const schdSetIsOverflowing = rafSchd(setIsOverflowing);
+
+  const handleResize = () => {
+    schdSetIsOverflowing(isElementOverflowing(contentEl));
+  };
+
+  useEffect(() => {
+    handleResize(); // needs to fire once immediately on mount
+    window.addEventListener('resize', handleResize);
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
+  return (
+    <AlertDialog.Root open={isOpen} onOpenChange={onDismiss}>
+      <AlertDialog.Overlay className="dialog-overlay"></AlertDialog.Overlay>
+      <div className="dialog-zIndex dialog-wrapper">
+        <AlertDialog.Content
+          // onPointerDownOutside={(event) => event.preventDefault()}
+          asChild
+          className="ease-in-out"
+        >
+          <div
+            className="dialog elevation--3 border--focus--noTransition"
+            role={'alertdialog'}
+            aria-labelledby={title && 'a11y-dialog-title'}
+            aria-describedby="a11y-dialog-desc"
+            tabIndex={-1}
+            aria-modal="true"
+            style={{
+              maxWidth: '500px',
+              maxHeight: '80vh',
+            }}
+            data-testid="dialog"
+          >
+            {(title || onDismiss) && (
+              <>
+                <div className="dialog-header">
+                  <div className="bgColor--white padding--all border--bottom">
+                    <div>
+                      <div className="padding--right--xl type--head4">
+                        {title ? <span id="a11y-dialog-title">{title}</span> : '\u00A0'}{' '}
+                        {/* There always needs to be something (even a space) in the header for display reasons */}
+                      </div>
+                    </div>
+                    {onDismiss && (
+                      <AlertDialog.Trigger asChild>
+                        <div className="dialog-icon">
+                          <IconButton Icon={DenyIcon} aria-label="Close" label="Close" />
+                        </div>
+                      </AlertDialog.Trigger>
+                    )}
+                  </div>
+                </div>
+              </>
+            )}
+            {desc && (
+              <div className="dialog-content" ref={contentEl}>
+                <div className="padding--all bgColor--white">
+                  {desc ? <div id="a11y-dialog-desc">{desc}</div> : null}
+                </div>
+              </div>
+            )}
+            {
+              <div className="dialog-footer">
+                <div
+                  className={cc([
+                    {
+                      'border--top': isOverflowing,
+                    },
+                    'bgColor--white',
+                    'padding--all',
+                  ])}
+                >
+                  <Flex justify="end">
+                    {secondaryButton && <FlexItem shrink>{secondaryButton}</FlexItem>}
+                    {primaryButton && <FlexItem shrink>{primaryButton}</FlexItem>}
+                  </Flex>
+                </div>
+              </div>
+            }
+          </div>
+        </AlertDialog.Content>
+      </div>
+    </AlertDialog.Root>
+  );
+};
 
 export default Prompt;
