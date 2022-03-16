a50f1dfd1986d32a5db51243957f7864b941b506
Iván Hernández <ivan.hernandez@cbinsights.com>
2022-03-16 13:15:36 -0600

    fix(header): remove unwanted files

--- src/components/TopSearch/CustomSelectComponents/CustomInput.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { components, InputProps } from 'react-select';
-import { SelectProps } from '../Search';
-
-const CustomInput = (props: InputProps<SelectProps, true>): JSX.Element => (
-  <components.Input {...props} isHidden={false} />
-);
-
-export default CustomInput;
