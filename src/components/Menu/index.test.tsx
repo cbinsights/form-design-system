58973acd63d9b52241f361e32e3df349d019f7e3
Iván Hernández <ivan.hernandez@cbinsights.com>
2022-04-05 09:49:53 -0500

    test(menu): increase coverage

--- src/components/Menu/index.test.tsx
+++ src/components/Menu/index.test.tsx
@@ -1,17 +1,13 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
-
-import { MenuItem } from 'components';
-import Menu from '.';
+import { render, screen, waitFor } from '@testing-library/react';
+import { Menu, MenuItem } from 'components';
+import userEvent from '@testing-library/user-event';
+// import { debugTest } from 'testing-library-visualizer';
 
 describe('Menu component', () => {
-  /**
-   * `@reach/menu-button` has its own test coverage.
-   * FDS provides only styling overrides, so snapshots will be adequate coverage for our code.
-   */
   it('renders component (strings)', () => {
     render(
-      <Menu trigger={<a href="#">trigger</a>}>
+      <Menu trigger={<a href="#">trigger</a>} defaultOpen>
         <MenuItem onSelect={jest.fn()}>First</MenuItem>
         <MenuItem onSelect={jest.fn()}>Second</MenuItem>
         <MenuItem onSelect={jest.fn()}>Third</MenuItem>
@@ -21,9 +17,10 @@ describe('Menu component', () => {
     expect(screen.getByText('Second')).toBeInTheDocument();
     expect(screen.getByText('Third')).toBeInTheDocument();
   });
+
   it('renders component (JSX items)', () => {
     render(
-      <Menu trigger={<a href="#">trigger</a>}>
+      <Menu trigger={<a href="#">trigger</a>} defaultOpen>
         <MenuItem onSelect={jest.fn()}>
           <p>First</p>
         </MenuItem>
@@ -39,6 +36,7 @@ describe('Menu component', () => {
     expect(screen.getByText('Second')).toBeInTheDocument();
     expect(screen.getByText('Third')).toBeInTheDocument();
   });
+
   it('renders trigger element only', () => {
     const mockOnClick = jest.fn();
     render(
@@ -55,11 +53,26 @@ describe('Menu component', () => {
         <MenuItem onSelect={jest.fn()}>Third</MenuItem>
       </Menu>
     );
-    const anchor = screen.getByRole('link');
-    expect(anchor).toBeInTheDocument();
+    expect(screen.getByRole('link')).toBeInTheDocument();
     // MenuItems are not expected to be rendered so we check for them to be null
     expect(screen.queryByText('First')).toBeNull();
     expect(screen.queryByText('Second')).toBeNull();
     expect(screen.queryByText('Third')).toBeNull();
+  });
+});
+
+it('shows content on Menu open', async () => {
+  render(
+    <Menu trigger={<a href="#">trigger</a>}>
+      <MenuItem onSelect={jest.fn()}>First</MenuItem>
+      <MenuItem onSelect={jest.fn()}>Second</MenuItem>
+      <MenuItem onSelect={jest.fn()}>Third</MenuItem>
+    </Menu>
+  );
+  userEvent.type(screen.getByRole('link', { name: 'trigger' }), '{arrowdown}');
+  await waitFor(() => {
+    expect(screen.getByText('First')).toBeInTheDocument();
+    expect(screen.getByText('Second')).toBeInTheDocument();
+    expect(screen.getByText('Third')).toBeInTheDocument();
   });
 });
