c080a798540020e5dde3be600f7bfe529d5451da
David Cohen <david.cohen@cbinsights.com>
2022-05-02 14:56:37 -0400

    chore(deps): disable eslint rule

--- fileTransformer.js
+++ fileTransformer.js
@@ -1,7 +1,9 @@
+/* eslint-disable no-undef */
+// eslint-disable-next-line @typescript-eslint/no-var-requires
 const path = require('path');
 
 module.exports = {
-  process(sourceText, sourcePath, options) {
+  process(sourceText, sourcePath, _options) {
     return {
       code: `module.exports = ${JSON.stringify(path.basename(sourcePath))};`,
     };
