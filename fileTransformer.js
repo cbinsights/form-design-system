8536d0e60cdd5688cda15ae66875b993537eac7e
David Cohen <david.cohen@cbinsights.com>
2022-05-02 14:57:12 -0400

    chore(deps): prefix with underscore

--- fileTransformer.js
+++ fileTransformer.js
@@ -3,7 +3,7 @@
 const path = require('path');
 
 module.exports = {
-  process(sourceText, sourcePath, _options) {
+  process(_sourceText, sourcePath, _options) {
     return {
       code: `module.exports = ${JSON.stringify(path.basename(sourcePath))};`,
     };
