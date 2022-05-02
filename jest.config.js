4baf6def9e509b45c46936e4b9afb0dd5d69d650
David Cohen <david.cohen@cbinsights.com>
2022-05-02 15:03:09 -0400

    chore(deps): add uuid to transformIgnore

--- jest.config.js
+++ jest.config.js
@@ -3,6 +3,7 @@ module.exports = {
   moduleDirectories: ['src'],
   rootDir: './',
   testPathIgnorePatterns: ['<rootDir>/lib/'],
+  transformIgnorePatterns: ['/node_modules/(?!(uuid|nanoid)/)'],
   coveragePathIgnorePatterns: ['<rootDir>/src/icons/react'],
   setupFilesAfterEnv: ['<rootDir>/scripts/node/setupTests.js', 'jest-prop-type-error'],
   testRegex: '(/__tests__/.*|(\\.|/)(test))\\.[jt]sx?$',
