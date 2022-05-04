8acf5b5edddcabeb272b16ca376dbb36e11227c6
ivanhdez28 <69323251+ivanhdez28@users.noreply.github.com>
2022-05-04 11:42:24 -0500

    fix(header): fix types mismatch introduced by having `isMulti` as a prop (#1172)
    Co-authored-by: Iván Hernández <ivan.hernandez@cbinsights.com>

--- src/components/SmartInput/CustomSelectComponents/CustomMenuList.tsx
+++ src/components/SmartInput/CustomSelectComponents/CustomMenuList.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { components, MenuListProps } from 'react-select';
 import { OptionProps } from '..';
 
-const CustomMenuList = (props: MenuListProps<OptionProps>): JSX.Element => {
+const CustomMenuList = (props: MenuListProps<OptionProps, boolean>): JSX.Element => {
   const innerProps = { ...props.innerProps, role: 'listbox' };
   return <components.MenuList {...props} innerProps={innerProps} />;
 };
